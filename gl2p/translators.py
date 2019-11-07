1ab00cfa120d62feb8453b73be1cf4c5a2c63dda
cdboer <cdboer@cdboer.de>
2019-11-07 16:29:34 +0100

    Rework file version discovery. Based on git graph (#4)

--- gl2p/translators.py
+++ gl2p/translators.py
@@ -1,5 +1,5 @@
 from collections import defaultdict, deque, namedtuple
-import prov.model as prov
+import prov.model as pm
 from prov.constants import PROV_LABEL, PROV_ROLE
 from gl2p.commons import FileAction
 
@@ -7,17 +7,16 @@ from gl2p.commons import FileAction
 class Translator:
 
     def __init__(self):
-        self.prov = prov.ProvDocument()
+        self.prov = None
 
     def translate(self, data, *args, **kwargs):
-        """
-        Abstract method to translate data of some kind to prov vocabulary
-        """
         raise NotImplementedError()
 
     def clean(self):
         # NOTE: unification removes duplicates by deleting multiplicities with same identifier
         # can also be used to remove duplicated relations, iff relation has identifier
+        if not isinstance(self.prov, pm.ProvDocument):
+            raise TypeError
         self.prov = self.prov.unified()
 
 
@@ -25,50 +24,126 @@ class CommitTranslator(Translator):
 
     def __init__(self):
         super().__init__()
-        # TODO: correct namespace definition for project
-        self.prov.set_default_namespace("https://example.org")
 
     def translate(self, commits, *args, **kwargs):
-        # compute stem file lookup
-        alias_lookup = self._compute_alias_lookup(commits)
+        # document setup
+        prov = pm.ProvDocument()
+        prov.set_default_namespace("https://example.org")
+
+        # compute lookups: somewhat memory intensive, maybe cut off waste?
+        al = self._compute_alias_lookup(commits)
+        cl = self._compute_commit_lookup(commits)
+
+        for commit in cl.values():
+            nmsp = self._format_names(commit)
+            prov.activity(
+                    identifier=nmsp.commit,
+                    other_attributes={PROV_LABEL: commit.message})
+            prov.wasStartedBy(
+                    nmsp.commit,
+                    time=commit.authored_date,
+                    identifier=nmsp.commit+commit.authored_date)
+            prov.wasEndedBy(
+                    nmsp.commit,
+                    time=commit.committed_date,
+                    identifier=nmsp.commit+commit.committed_date)
 
         for commit in commits:
-            names = self._format_names(commit)
+            nmsp = self._format_names(commit)
+            for parent in nmsp.parents:
+                prov.wasInformedBy(
+                        nmsp.commit,
+                        parent,
+                        identifier=nmsp.commit+parent)
+            prov.agent(identifier=nmsp.author)
+            prov.agent(identifier=nmsp.committer)
+            prov.wasAssociatedWith(
+                    nmsp.commit,
+                    nmsp.author,
+                    other_attributes={PROV_ROLE: "author"})
+            prov.wasAssociatedWith(
+                    nmsp.commit,
+                    nmsp.committer,
+                    other_attributes={PROV_ROLE: "committer"})
 
-            self.prov.wasStartedBy(names.commit, time=commit.authored_date)
-            self.prov.wasEndedBy(names.commit, time=commit.committed_date)
-            self.prov.activity(identifier=names.commit, other_attributes={PROV_LABEL: commit.message})
-            
-            # NOTE: wasInformedBy relation documents the chain of commits
-            # much like a commit graph
-            for parent in names.parents:
-                self.prov.wasInformedBy(names.commit, parent, identifier=names.commit+parent)
-            
-            # NOTE: stem file tracking allows mapping of a file alias to its original file entity
-            stem_file = alias_lookup[commit.file_path_used]
-            self.prov.agent(names.author)
-            self.prov.agent(names.committer)
-            self.prov.wasAssociatedWith(names.commit, names.author, other_attributes={PROV_ROLE: "author"})
-            self.prov.wasAssociatedWith(names.commit, names.committer, other_attributes={PROV_ROLE: "committer"})
-            
-            # NOTE: depending on the file action, decide what prov vocabulary to add
-            if commit.file_action == FileAction.ADDED:
-                self.prov.entity(names.file, other_attributes={PROV_LABEL: commit.file_path_used})
-            if commit.file_action == FileAction.MODIFIED:
-                # TODO: fix issue with merge commits.
-                # what files should be "used"
-                # what files should be "derived"
-                for used_file in names.used_files:
-                    self.prov.used(names.commit, used_file, time=commit.authored_date)
-                    self.prov.wasDerivedFrom(names.generated_file, used_file, names.commit)
+            used_files = self._search_used_versions(al, cl, commit)
             if commit.file_action == FileAction.DELETED:
-                for used_file in names.used_files:
-                    self.prov.wasInvalidatedBy(used_file, names.commit, time=commit.authored_date)
+                for cid in used_files:
+                    prov.wasInvalidatedBy(
+                            f"file-{self._namify(cl[cid].generated)}_commit-{cid}",
+                            nmsp.commit,
+                            time=commit.authored_date)
             else:
-                self.prov.specializationOf(names.generated_file, f"file-{self._namify(stem_file)}")
-                self.prov.wasGeneratedBy(names.generated_file, names.commit, time=commit.authored_date)
-                self.prov.entity(identifier=names.generated_file)
-                self.prov.wasAttributedTo(names.generated_file, names.author)
+                prov.entity(identifier=nmsp.generated)
+                prov.wasAttributedTo(nmsp.generated, nmsp.author)
+                stem_file = al[commit.used]
+                prov.specializationOf(
+                        nmsp.generated,
+                        f"file-{self._namify(stem_file)}")
+                prov.wasGeneratedBy(
+                        nmsp.generated,
+                        nmsp.commit,
+                        time=commit.authored_date)
+            if commit.file_action == FileAction.ADDED:
+                prov.entity(
+                        nmsp.file,
+                        other_attributes={PROV_LABEL: commit.used})
+            if commit.file_action == FileAction.MODIFIED:
+                for cid in used_files:
+                    prov.used(
+                            nmsp.commit,
+                            f"file-{self._namify(cl[cid].generated)}_commit-{cid}",
+                            time=commit.authored_date)
+                    prov.wasDerivedFrom(
+                            nmsp.generated,
+                            f"file-{self._namify(cl[cid].generated)}_commit-{cid}",
+                            nmsp.commit)
+        self.prov = prov
+
+    def _compute_commit_lookup(self, commits):
+        cl = dict()
+        for commit in commits:
+            if not cl.get(commit.id, False):
+                commit.files = [(commit.used, commit.generated)]
+                cl[commit.id] = commit
+                continue
+            origin = cl.get(commit.id)
+            if not hasattr(origin, "files"):
+                origin.files = [(origin.used, origin.generated)]
+            origin.files.append((commit.used, commit.generated))
+            cl[commit.id] = origin
+        return cl
+
+    def _search_used_versions(self, al, cl, c):
+        # TODO: i don't think this is robust yet.
+        # The following case yields a wrong result searching for F
+        #        * -- F
+        #        |
+        #      /  \
+        #     /    *--F'
+        # G--*     |
+        #    \     |
+        #     \   /
+        #      \ /
+        #       *
+        # RESULT: F, F'
+        stack, visited = [c.id], []
+        res = []
+        if c.file_action == FileAction.ADDED:
+            return []
+        while stack:
+            active = stack.pop()
+            if active in visited:
+                continue
+            visited.append(active)
+            for _, gen in cl.get(active).files:
+                if active != c.id and al[gen] == al[c.used]:
+                    res.append(active)
+                    break
+            else:
+                for parent in cl[active].parent_ids:
+                    stack.append(parent)
+        return res
 
     def _namify(self, string):
         replacements = {"-": ["/", ".", " "]}
@@ -78,23 +153,22 @@ class CommitTranslator(Translator):
         return string
 
     def _format_names(self, commit):
-        namespace = "commit parents author committer file generated_file used_files"
+        namespace = "commit parents author committer file generated"
         Names = namedtuple("Names", namespace)
         return Names(*[
             f"commit-{commit.id}",
             [f"commit-{pid}" for pid in commit.parent_ids],
             f"user-{self._namify(commit.author_name)}",
             f"user-{self._namify(commit.committer_name)}",
-            f"file-{self._namify(commit.file_path_used)}",
-            f"file-{self._namify(commit.file_path_generated)}_commit-{commit.id}",
-            [f"file-{self._namify(commit.file_path_used)}_commit-{pid}" for pid in commit.parent_ids]])
+            f"file-{self._namify(commit.used)}",
+            f"file-{self._namify(commit.generated)}_commit-{commit.id}"])
 
     def _compute_alias_lookup(self, commits):
         track = defaultdict(set)  # file alias records
         files = set()  # list of filenames
         for commit in commits:
-            files |= set([commit.file_path_used, commit.file_path_generated])
-            track[commit.file_path_used].add(commit.file_path_generated)
+            files |= set([commit.used, commit.generated])
+            track[commit.used].add(commit.generated)
 
         # explore file records by bfs to compute file tracks
         roots = []
