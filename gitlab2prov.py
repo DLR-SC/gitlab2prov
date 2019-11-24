0f7ae608a039eff3d0a0ccc642f9520aa2fe2a28
cdboer <cdboer@cdboer.de>
2019-11-24 13:34:38 +0100

    Fix import

--- gitlab2prov.py
+++ gitlab2prov.py
@@ -6,7 +6,7 @@ from gl2p.commons import URLException
 from gl2p.config import CONFIG
 
 from provdbconnector import ProvDb
-from provdbconnector.db_adapters.neo4j.neo4jadapter import Neo4jAdapter
+from provdbconnector import Neo4jAdapter
 from prov.dot import prov_to_dot
 
 
@@ -19,12 +19,7 @@ def drain_pipelines():
         prov = pipe.commit_data()
     return prov
 
-
 def to_neo4j(prov):
-    # TODO: fix provdbconnector error
-    # provdbconnector.exceptions.database.MergeException:
-    # The attributes [<QualifiedName: prov:role>, 'meta:prov_type', 'meta:identifier', 'meta:namespaces', 'meta:type_map']
-    # could not merged into the existing node
     auth_info = {
             "user_name": CONFIG["NEO4J"]["user"],
             "user_password": CONFIG["NEO4J"]["password"],
@@ -32,7 +27,6 @@ def to_neo4j(prov):
             }
     prov_api = ProvDb(adapter=Neo4jAdapter, auth_info=auth_info)
     prov_api.save_document(prov)
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="GitLab repo to prov doc")
@@ -47,4 +41,4 @@ if __name__ == "__main__":
     dotfile = open("prov.dot", "w")
     print(prov_to_dot(prov), file=dotfile)
     print(prov)
-    # to_neo4j(prov)
+    to_neo4j(prov)
