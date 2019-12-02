9f72c819cac30a4bf95664fdccceeeb2f4029a70
Claas de Boer <cdboer@cdboer.de>
2019-12-02 16:21:29 +0100

    Rework main(), add preliminary workflow impl

--- gitlab2prov.py
+++ gitlab2prov.py
@@ -1,32 +1,72 @@
 #!/usr/bin/env python
-import argparse
-from gl2p.pipes import CommitPipeline
-from gl2p.utils import url_validator
-from gl2p.commons import URLException
-from gl2p.config import CONFIG
 
+# stdlib imports
+from urllib.parse import urlparse
+import argparse
+import subprocess
+# third party imports
 from provdbconnector import ProvDb
 from provdbconnector import Neo4jAdapter
 from prov.dot import prov_to_dot
+# local imports
+from gl2p.pipes import CommitPipeline
+from gl2p.helpers import url_validator
+from gl2p.commons import URLException
+from gl2p.config import CONFIG
+from gl2p.origin import GitLabOrigin
+from gl2p.translator import Translator
 
 
-def drain_pipelines():
-    pipelines = [CommitPipeline()]
-    for pipe in pipelines:
-        pipe.request_data()
-        pipe.process_data()
-        pipe.translate_data()
-        prov = pipe.commit_data()
-    return prov
+class Workflow:
 
-def to_neo4j(prov):
-    auth_info = {
-            "user_name": CONFIG["NEO4J"]["user"],
-            "user_password": CONFIG["NEO4J"]["password"],
-            "host": CONFIG["NEO4J"]["host"]
-            }
-    prov_api = ProvDb(adapter=Neo4jAdapter, auth_info=auth_info)
-    prov_api.save_document(prov)
+    def __init__(self):
+        self.data = None
+
+        self.source = None
+        self.translator = Translator()
+        self.connection_handler = None
+
+    def drain(self):
+        self.source.fetch()
+        self.data = self.source.process()
+
+    def translate(self):
+        return self.translator.run(self.data)
+
+    def commit(self):
+        raise NotImplementedError()
+
+
+def main():
+    # -- flow definition --
+    workflow = Workflow()
+
+    # -- choose correct source --
+    parsed = urlparse(args.url)
+    if "gitlab" in parsed.netloc:
+        workflow.source = GitLabOrigin()
+    else:
+        return
+
+    # -- work the source --
+    workflow.drain()
+    print("--» SOURCE DRAINED!")
+
+    # -- translate raw data --
+    document = workflow.translate()
+    print("-- Created document.")
+
+    # -- print to file --
+    # provdoc = open("provdoc", "w")
+    # print("-- Printing doc.")
+    # print(document.get_provn(), file=provdoc)
+    
+    # -- create dot file --
+    # dotfile = open("prov.dot", "w")
+    # print(prov_to_dot(document), file=dotfile)
+
+    # -- compute layout as pdf --
+    # subprocess.run(["dot","-Tpdf", "prov.dot", "-o", "prov.pdf"])
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="GitLab repo to prov doc")
@@ -36,9 +76,4 @@ if __name__ == "__main__":
     if not url_validator(args.url):
         raise URLException("URL invalid.")
     CONFIG["GITLAB"]["project"] = args.url
-
-    prov = drain_pipelines()
-    dotfile = open("prov.dot", "w")
-    print(prov_to_dot(prov), file=dotfile)
-    print(prov)
-    to_neo4j(prov)
+    main()
