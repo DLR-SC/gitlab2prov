a9bc838ebba21bb4f956628751b374f0c68f5ffc
Claas de Boer <cdboer@cdboer.de>
2019-12-06 14:42:06 +0100

    Correct git_commit to gitlab_commit

--- models/commit_model_delete.py
+++ models/commit_model_delete.py
@@ -9,18 +9,18 @@ document.entity("file")
 document.entity("file_v-1")
 document.entity("file_v")
 document.activity("parent_commit")
-document.activity("git_commit")
+document.activity("gitlab_commit")
 document.agent("author")
 document.agent("committer")
 
 # Relations
-document.wasInvalidatedBy("file_v", "git_commit")
-document.wasInformedBy("git_commit", "parent_commit")
+document.wasInvalidatedBy("file_v", "gitlab_commit")
+document.wasInformedBy("gitlab_commit", "parent_commit")
 document.specializationOf("file_v", "file")
 document.specializationOf("file_v-1", "file")
 document.wasDerivedFrom("file_v", "file_v-1")
-document.wasAssociatedWith("git_commit", "author")
-document.wasAssociatedWith("git_commit", "committer")
+document.wasAssociatedWith("gitlab_commit", "author")
+document.wasAssociatedWith("gitlab_commit", "committer")
 document.wasAttributedTo("file_v", "author")
 
 COMMIT_MODEL_DELETE = document
