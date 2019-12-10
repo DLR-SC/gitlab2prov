6ab8d3e06427177883560b596057eb37d384a801
Claas de Boer <cdboer@cdboer.de>
2019-12-10 13:41:56 +0100

    Add implementation of Commit Event Model, references to other models excluded.

--- gl2p/translator.py
+++ gl2p/translator.py
@@ -22,6 +22,23 @@ from gl2p.helpers import unique_id
 from gl2p.commons import FileStatus
 
 
+# NODE TYPES
+# USED TO DISTINGUISH ID NAMESPACES
+FILE_ORIGINAL = "FILE_ORIGINAL"
+FILE_VERSION = "FILE_VERSION"
+COMMIT_ENTITY = "COMMIT_ENTITY"
+COMMIT_VERSION = "COMMIT_VERSION"
+COMMIT_ACTIVITY = "COMMIT_ACTIVITY"
+COMMIT_EVENT = "COMMIT_EVENT"
+ISSUE_EVENT = "ISSUE_EVENT"
+ISSUE_ENTITY = "ISSUE_ENTITY"
+ISSUE_VERSION = "ISSUE_VERSION"
+MERGE_REQUEST_ENTITY = "MERGE_REQUEST_ENTITY"
+MERGE_REQUEST_EVENT = "MERGE_REQUEST_EVENT"
+MERGE_REQUEST_VERSION = "MERGE_REQUEST_VERSION"
+USER = "USER"
+
+
 class Translator:
 
     def __init__(self):
@@ -36,10 +53,12 @@ class Translator:
         specialized = set()
 
         for commit, fil in ((c, f) for c in commits for f in c.attributes.get("files")):
-            c = commit.attributes.get("id")
-            f, fv = unique_id("ORIGINAL", nametables.get(c).get(fil.new_path)), unique_id(c, fil.new_path)
-            author = unique_id(commit.attributes.get("author_name"))
-            committer = unique_id(commit.attributes.get("committer_name"))
+            sha = commit.attributes.get("id")
+            c = unique_id([sha], prefix=COMMIT_ACTIVITY)
+            f = unique_id([nametables.get(sha).get(fil.new_path)], prefix=FILE_ORIGINAL)
+            fv = unique_id([c, fil.new_path], prefix=FILE_VERSION)
+            author = unique_id([commit.attributes.get("author_name")], prefix=USER)
+            committer = unique_id([commit.attributes.get("committer_name")], prefix=USER)
 
             if fil.status == FileStatus.ADDED:
                 # Nodes
@@ -49,18 +68,20 @@ class Translator:
                 document.agent(committer)
                 document.activity(c)
                 for parent in commit.attributes.get("parent_ids"):
+                    parent = unique_id([parent], prefix=COMMIT_ACTIVITY)
                     document.activity(parent)
                 # Relations
-                if fv not in specialized:
-                    document.specializationOf(fv, f) # id: version
-                    specialized.add(fv)
                 document.wasAttributedTo(fv, author)
                 document.wasAttributedTo(f, author)
                 document.wasAssociatedWith(c, author)
                 document.wasAssociatedWith(c, committer)
                 document.wasGeneratedBy(f, c)
                 document.wasGeneratedBy(fv, c)
+                if fv not in specialized:
+                    document.specializationOf(fv, f) # id: version
+                    specialized.add(fv)
                 for parent in commit.attributes.get("parent_ids"):
+                    parent = unique_id([parent], prefix=COMMIT_ACTIVITY)
                     document.wasInformedBy(c, parent, identifier=c+parent)
 
             if fil.status == FileStatus.MODIFIED:
@@ -70,28 +91,31 @@ class Translator:
                 document.agent(committer)
                 document.activity(c)
                 for parent in commit.attributes.get("parent_ids"):
-                    fv_prev = unique_id(parent, fil.old_path)
+                    parent = unique_id([parent], prefix=COMMIT_ACTIVITY)
+                    fv_prev = unique_id([parent, fil.old_path], prefix=FILE_VERSION)
                     document.activity(parent)
                     document.entity(fv_prev)
                 
                 # Relations
-                if fv not in specialized:
-                    document.specializationOf(fv, f)
-                    specialized.add(fv)
                 document.wasGeneratedBy(fv, c)
                 document.wasAssociatedWith(c, author)
                 document.wasAssociatedWith(c, committer)
                 document.wasAttributedTo(fv, author)
+                if fv not in specialized:
+                    document.specializationOf(fv, f)
+                    specialized.add(fv)
                 for parent in commit.attributes.get("parent_ids"):
-                    fv_prev = unique_id(parent, fil.old_path)
-                    fv_prev_original = unique_id("ORIGINAL", nametables.get(parent).get(fil.old_path))
+                    prev_original = nametables.get(parent).get(fil.old_path)
+                    parent = unique_id([parent], prefix=COMMIT_ACTIVITY)
+                    fv_prev = unique_id([parent, fil.old_path], prefix=FILE_VERSION)
+                    fv_prev_original = unique_id([prev_original], prefix=FILE_ORIGINAL)
                     document.entity(fv_prev_original)
                     document.used(c, fv_prev)
                     document.wasDerivedFrom(fv, fv_prev)
+                    document.wasInformedBy(c, parent, identifier=c+parent)
                     if fv_prev not in specialized:
                         document.specializationOf(fv_prev, fv_prev_original)
                         specialized.add(fv_prev)
-                    document.wasInformedBy(c, parent, identifier=c+parent)
 
             if fil.status == FileStatus.DELETED:
                 # Nodes
@@ -99,7 +123,8 @@ class Translator:
                 document.agent(author)
                 document.agent(committer)
                 for parent in commit.attributes.get("parent_ids"):
-                    fv_prev = unique_id(parent, fil.old_path)
+                    parent = unique_id([parent], prefix=COMMIT_ACTIVITY)
+                    fv_prev = unique_id([parent, fil.old_path], prefix=FILE_VERSION)
                     document.entity(fv_prev)
                     document.activity(parent)
 
@@ -107,16 +132,75 @@ class Translator:
                 document.wasAssociatedWith(c, author)
                 document.wasAssociatedWith(c, committer)
                 for parent in commit.attributes.get("parent_ids"):
-                    fv_prev = unique_id(parent, fil.old_path)
-                    fv_prev_original = unique_id("ORIGINAL", nametables.get(parent).get(fil.old_path))
+                    prev_original = nametables.get(parent).get(fil.old_path)
+                    parent = unique_id([parent], prefix=COMMIT_ACTIVITY)
+                    fv_prev = unique_id([parent, fil.old_path], prefix=FILE_VERSION)
+                    fv_prev_original = unique_id([prev_original], prefix=FILE_ORIGINAL)
                     document.entity(fv_prev_original)
+                    document.wasInvalidatedBy(fv_prev, c)
+                    document.wasInformedBy(c, parent, identifier=c+parent)
                     if fv_prev not in specialized:
                         document.specializationOf(fv_prev, fv_prev_original)
                         specialized.add(fv_prev)
-                    document.wasInformedBy(c, parent, identifier=c+parent)
-                    document.wasInvalidatedBy(fv_prev, c)
-        
-        # Commit Entity Model
+
+
+        # Commit Model - New Commit
+        for commit in commits:
+            ca = unique_id([commit.attributes.get("id")], prefix=COMMIT_ACTIVITY)
+            ce = unique_id([ca], prefix=COMMIT_ENTITY)
+            cv = unique_id([ca], prefix=COMMIT_VERSION)
+            committer = unique_id([commit.attributes.get("committer_name")], prefix=USER)
+            # Nodes
+            document.entity(ce)
+            document.entity(cv)
+            document.activity(ca)
+            document.agent(committer)
+            for parent in commit.attributes.get("parent_ids"):
+                parent = unique_id([parent], prefix=COMMIT_ACTIVITY)
+                document.activity(parent)
+            # Relations
+            document.wasGeneratedBy(ce, ca)
+            document.wasGeneratedBy(cv, ca)
+            document.wasAttributedTo(ce, committer)
+            document.wasAttributedTo(cv, committer)
+            document.wasAssociatedWith(ca, committer)
+            if cv not in specialized:
+                document.specializationOf(cv, ce)
+                specialized.add(cv)
+            for parent in commit.attributes.get("parent_ids"):
+                parent = unique_id([parent], prefix=COMMIT_ACTIVITY)
+                document.wasInformedBy(ca, parent, identifier=ca+parent)
+
+
+        # Commit Model - New Commit Event
+        for commit in commits:
+            sha = commit.attributes.get("id")
+            ca = unique_id([sha], prefix=COMMIT_ACTIVITY)
+            ce = unique_id([ca], prefix=COMMIT_ENTITY)
+            cv_prev = unique_id([ca], prefix=COMMIT_VERSION)
+            event_prev = ca
+            for event in commit.events:
+                initiator = unique_id([event.initiator], prefix=USER)
+                event = unique_id([event.id], prefix=COMMIT_EVENT)
+                cv = unique_id([ca, event], prefix=COMMIT_VERSION)
+                # Nodes
+                document.entity(ce)
+                document.entity(cv)
+                document.entity(cv_prev)
+                document.activity(event)
+                document.agent(initiator)
+                # Relations
+                document.wasDerivedFrom(cv, cv_prev)
+                document.wasGeneratedBy(cv, event)
+                document.used(event, cv_prev)
+                document.wasAttributedTo(cv, initiator)
+                document.wasAssociatedWith(event, initiator)
+                document.wasInformedBy(event, event_prev)
+                if cv not in specialized:
+                    document.specializationOf(cv, ce)
+                # Update prev values
+                cv_prev = cv
+                event_prev = event
 
         document = document.unified()
         return document
