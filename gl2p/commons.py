71c8ce4658cf09078f53fc262259ccc01f8d87c0
Claas de Boer <cdboer@cdboer.de>
2020-01-21 14:09:58 +0100

    Remove legacy implementation of Events, NameTables

--- gl2p/commons.py
+++ gl2p/commons.py
@@ -17,21 +17,17 @@
 import re
 from copy import deepcopy
 from dataclasses import dataclass, field
+from datetime import datetime
 from collections.abc import MutableMapping
-from typing import List, NamedTuple, Dict
+from collections import namedtuple
+from functools import total_ordering
+from typing import List, NamedTuple, Dict, Any
 from enum import Enum
-from gitlab.v4.objects import ProjectCommit, ProjectIssue
-
-
-def URLException(Exception):
-    pass
-
-
-def ConfigurationException(Exception):
-    pass
 
 
 class FileStatus(Enum):
+    """Action that diff entry represents."""
+
     ADDED = "A"
     MODIFIED = "M"
     DELETED = "D"
@@ -39,105 +35,34 @@ class FileStatus(Enum):
 
 @dataclass
 class File:
-    commit_sha: str
-    old_path: str
-    new_path: str
-    status: FileStatus
+    """A repository file version."""
 
+    old_path: str = ""
+    new_path: str = ""
+    status: FileStatus = FileStatus.ADDED
 
-class RepositoryInfo(NamedTuple):
-    path: str
-       
+    db_id: str = ""
+    prev_version_ids: List[str] = ""
+    original: str = ""
 
-class NameTable(MutableMapping):
+    @staticmethod
+    def _file_status(tup):
+        """Determine action occuring for diffed file."""
 
-    def __init__(self, *args, **kwargs):
-        self.store = dict()
-        self.update(dict(*args, **kwargs))
+        new, deleted = tup
+        if new:
+            return FileStatus.ADDED
+        elif deleted:
+            return FileStatus.DELETED
 
-    def __getitem__(self, key):
-        return self.store[key]
+        return FileStatus.MODIFIED
 
-    def __setitem__(self, key, value):
-        self.store[key] = value
+    @classmethod
+    def from_diff(cls, diff):
+        """Create File from diff entry."""
 
-    def __delitem__(self, key):
-        del self.store[key]
+        op, np = diff.get("old_path"), diff.get("new_path")
+        nf, df = diff.get("new_file"), diff.get("deleted_file")
+        status = cls._file_status((nf, df))
 
-    def __iter__(self):
-        return iter(self.store)
-
-    def __len__(self):
-        return len(self.store)
-
-    def __repr__(self):
-        return str(self.store)
-
-    def __str__(self):
-        return "Nametable: {}".format(self.__repr__())
-
-    def __add__(self, other):
-        store = deepcopy(self.store)
-        store.update(other)
-        return NameTable(store)
-
-    def delta(self, diff):
-        # replay changes from diff on a nametable
-        store = deepcopy(self.store)
-        for entry in diff:
-            new, old = entry.get("new_path"), entry.get("old_path")
-            if new != old:
-                # file moved or renamed
-                if old in store:
-                    # update existing
-                    root = store.get(old)
-                    store[new] = root
-                else:
-                    # update/add
-                    store[new] = old
-            if entry.get("new_file"):
-                store[new] = old
-            elif entry.get("deleted_file"):
-                pass
-            else:
-                # modified file
-                if new not in store:
-                    store[new] = old
-
-        return NameTable(store)             
-
-
-
-class CommitEvent:
-
-    def __init__(self, note):
-        self.id = note.get("id")
-        self.body = note.get("body", "")
-        self.initiator = note.get("author").get("name")
-        self.reference = None
-        self.target = self.conclude_target(note)
-        self.date = note.get("created_at")
-
-    def conclude_target(self, note):
-        if not note.get("system"):
-            return ""
-        if "commit" in self.body:
-            self.reference = "COMMIT"
-            return self.body.split(" ")[-1]
-        elif "issue" in self.body:
-            self.reference = "ISSUE"
-        elif "merge request" in self.body:
-            self.reference = "MERGE_REQUEST"
-        return re.findall(r'\d+', self.body)[0]
-
-        
-@dataclass
-class Repository:
-
-    info: RepositoryInfo 
-    commits: List
-    issues: List
-    nametables: Dict[str, NameTable]
-
-    def __str__(self):
-        return f"{self.info}\n{self.commits}\n{self.issues}\n{self.nametables}"
+        return cls(op, np, status)
