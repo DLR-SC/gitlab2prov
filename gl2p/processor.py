3dba6128cc7ca8bcfa2b286e4166207339ed55d4
Claas de Boer <cdboer@cdboer.de>
2020-01-29 12:21:11 +0100

    Refactor gl2p/processor.py (#25)
    Refactor to adhere to flake8 linter specification.
    Linter run with the following config:
    flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
    
    Changes:
    - remove unused imports
    - remove whitespace from blank lines
    - remove trailing whitespace
    - add linebreaks for long method headers

--- gl2p/processor.py
+++ gl2p/processor.py
@@ -15,17 +15,17 @@
 # code-author: Claas de Boer <claas.deboer@dlr.de>
 
 
-
-from copy import deepcopy
-from dataclasses import InitVar, asdict, dataclass, field
-from typing import Any, Dict, Iterable, List, Optional, Tuple, Union
+from dataclasses import dataclass
+from typing import Any, Dict, List, Optional, Tuple, Union
 
 from gl2p.commons import File, FileStatus
 from gl2p.eventparser import EventParser
 from gl2p.helpers import by_date, ptime, qname
-from gl2p.objects import *
 from gl2p.register import FileNameRegister
-from prov.constants import PROV_LABEL, PROV_ROLE, PROV_TYPE
+from prov.constants import PROV_ROLE, PROV_TYPE
+from gl2p.objects import (Commit, PROVNode, PROVActivity, Addition,
+                          Deletion, Modification, Resource,
+                          CommitResourceCreation, Event)
 
 
 @dataclass
@@ -33,14 +33,13 @@ class CommitProcessor:
     """
     Turn GitLab Commit and Diff API Resources into PROV vocabulary.
     """
-
     project_id: str
     file_register: Optional[FileNameRegister] = None
     commit_lookup: Optional[Dict[str, Any]] = None
 
     def run(self, commits: List[Dict[str, Any]], diffs: List[Any]) -> List[Commit]:
         """
-        Return list of objects.Commit ready to insert into model. 
+        Return list of objects.Commit ready to insert into model.
 
         Precompute labels and id's.
         """
@@ -66,10 +65,10 @@ class CommitProcessor:
         """
         id_ = qname(f"user-{c.get('author_name')}")
         label = {
-            PROV_TYPE: "user", 
+            PROV_TYPE: "user",
             PROV_ROLE: "author",
             "name": c.get("author_name")
-        } 
+        }
         return PROVNode(id_, label)
 
     def committer(self, c: Dict[str, Any]) -> PROVNode:
@@ -78,7 +77,7 @@ class CommitProcessor:
         """
         id_ = qname(f"user-{c.get('committer_name')}")
         label = {
-            PROV_TYPE: "user", 
+            PROV_TYPE: "user",
             PROV_ROLE: "committer",
             "name": c.get('committer_name')
         }
@@ -92,9 +91,9 @@ class CommitProcessor:
         start = ptime(c.get("authored_date"))
         end = ptime(c.get("committed_date"))
         label = {
-            PROV_TYPE: "commit", 
-            "sha": c.get("id"), 
-            "title": c.get("title"), 
+            PROV_TYPE: "commit",
+            "sha": c.get("id"),
+            "title": c.get("title"),
             "message": c.get("message")
         }
         return PROVActivity(id_, start, end, label)
@@ -124,7 +123,7 @@ class CommitProcessor:
 
     def file_changes(self, c: Dict[str, Any], df: List[Dict[str, Any]]) -> List[Union[Addition, Deletion, Modification]]:
         """
-        Returns list of file changes. 
+        Returns list of file changes.
 
         Addition, Modification or Deletion parsed from entries of commit diff *df* of commit *c*.
         """
@@ -137,16 +136,16 @@ class CommitProcessor:
             if parsed.status == FileStatus.ADDED:
                 id_ = qname(f"file-{original}")
                 label = {
-                    PROV_TYPE: "file", 
-                    "old_path": parsed.old_path, 
+                    PROV_TYPE: "file",
+                    "old_path": parsed.old_path,
                     "new_path": parsed.new_path
                 }
                 f = PROVNode(id_, label)
-                    
+
                 id_ = qname("file-version-{original}-{c.get('id')}")
                 label = {
-                    PROV_TYPE: "file_version", 
-                    "new_path": parsed.new_path, 
+                    PROV_TYPE: "file_version",
+                    "new_path": parsed.new_path,
                     "old_path": parsed.old_path
                 }
                 fv = PROVNode(id_, label)
@@ -156,11 +155,11 @@ class CommitProcessor:
                 id_ = qname(f"file-{original}")
                 label = {PROV_TYPE: "file"}
                 f = PROVNode(id_, label)
-                    
+
                 id_ = qname(f"file-version-{original}-{c.get('id')}")
                 label = {
-                    PROV_TYPE: "file_version", 
-                    "new_path": parsed.new_path, 
+                    PROV_TYPE: "file_version",
+                    "new_path": parsed.new_path,
                     "old_path": parsed.old_path
                 }
                 fv = PROVNode(id_, label)
@@ -170,18 +169,18 @@ class CommitProcessor:
                 id_ = qname(f"file-{original}")
                 label = {PROV_TYPE: "file"}
                 f = PROVNode(id_, label)
-                
+
                 id_ = qname(f"file-version-{original}-{c.get('id')}")
                 label = {
-                    PROV_TYPE: "file_version", 
-                    "new_path": parsed.new_path, 
+                    PROV_TYPE: "file_version",
+                    "new_path": parsed.new_path,
                     "old_path": parsed.old_path
                 }
                 fv = PROVNode(id_, label)
-                
+
                 label = {PROV_TYPE: "file_version"}
                 fv_1s = [
-                    PROVNode(qname(f"file-version-{original}-{pid}"), label) 
+                    PROVNode(qname(f"file-version-{original}-{pid}"), label)
                     for pid in c.get("parent_ids")
                 ]
                 files.append(Modification(f, fv, fv_1s))
@@ -191,7 +190,9 @@ class CommitProcessor:
 
 @dataclass
 class CommitResourceProcessor:
-
+    """
+    Prepare commits and commit notes to add to prov document by precomputing labels and id's.
+    """
     project_id: str
 
     def run(self, commits: List[Dict[str, Any]], notes: List[List[Dict[str, Any]]]) -> List[Resource]:
@@ -219,7 +220,7 @@ class CommitResourceProcessor:
         """
         id_ = qname(f"user-{c.get('committer_name')}")
         label = {
-            PROV_TYPE: "user", 
+            PROV_TYPE: "user",
             PROV_ROLE: "event_initiator",
             "name": c.get("committer_name")
         }
@@ -233,7 +234,7 @@ class CommitResourceProcessor:
         start = ptime(c.get('authored_date'))
         end = ptime(c.get('committed_date'))
         label = {
-            PROV_TYPE: "commit", 
+            PROV_TYPE: "commit",
             "sha": c.get('id'),
             "title": c.get('title'),
             "message": c.get("message")
@@ -249,7 +250,7 @@ class CommitResourceProcessor:
         end = ptime(c.get("committed_date"))
         label = {PROV_TYPE: "commit_resource_creation"}
         return PROVActivity(id_, start, end, label)
-        
+
     def resource(self, c: Dict[str, Any]) -> Tuple[PROVNode, PROVNode]:
         """
         Return tuple of resource, resource version for resource creation of commit *c*.
@@ -257,14 +258,15 @@ class CommitResourceProcessor:
         id_ = qname(f"commit-resource-{c.get('id')}")
         label = {PROV_TYPE: "commit_resource"}
         r = PROVNode(id_, label)
-            
+
         id_ = qname(f"commit-resource-{c.get('id')}-original")
         label = {PROV_TYPE: "commit_resource_version"}
         rv = PROVNode(id_, label)
 
         return r, rv
 
-    def events(self, c: Dict[str, Any], notes: List[Dict[str, Any]], eprev: PROVActivity, r:PROVNode,  rv_1: PROVNode) -> List[Event]:
+    def events(self, c: Dict[str, Any], notes: List[Dict[str, Any]],
+               eprev: PROVActivity, r: PROVNode,  rv_1: PROVNode) -> List[Event]:
         """
         Returns list of events that occur on commit *c* ordered by creation date asc.
         """
@@ -273,17 +275,17 @@ class CommitResourceProcessor:
 
         for note in notes:
             parsed = EventParser.parse_note(note)
-            
+
             id_ = qname(f"user-{parsed.initiator}")
             label = {PROV_TYPE: "user", "name": parsed.initiator}
             initiator = PROVNode(id_, label)
-            
+
             id_ = qname(f"commit-resource-event-{parsed.identifier}")
             start = ptime(parsed.created_at)
             end = ptime(parsed.created_at)
             label = {PROV_TYPE: "commit_event", "event_type": parsed.type}
             event = PROVActivity(id_, start, end, label)
-            
+
             id_ = qname(f"{r.id}-version-{parsed.identifier}")
             label = {PROV_TYPE: "commit_resource_version"}
             rv = PROVNode(id_, label)
