ba37c0ca38f1ee79118e641f96415463022ab0f8
Claas de Boer <cdboer@cdboer.de>
2020-02-12 20:26:45 +0100

    Add PROV-DM groupings with type hints
    Contents:
    - Add eventable stores in EventableCandidates and
        EventableCandidateContainer
    - Add PROV-DM groupings
    - Rename PROV-DM base types to better reflect their task
    
    See also: #25, #26

--- gl2p/utils/objects.py
+++ gl2p/utils/objects.py
@@ -14,94 +14,192 @@
 #
 # code-author: Claas de Boer <claas.deboer@dlr.de>
 
-
+from dataclasses import dataclass, field, asdict
 import datetime
-from typing import List, Dict, Union, NamedTuple
+from typing import NamedTuple, Union, Dict, List, Any, Iterable, Tuple
 
 
-class PROVNode(NamedTuple):
+APIResource = Dict[str, Any]
+
+
+@dataclass
+class EventCandidates:
+
+    labels: List[APIResource] = field(default_factory=list)
+    awards: List[APIResource] = field(default_factory=list)
+    notes: List[APIResource] = field(default_factory=list)
+    note_awards: List[APIResource] = field(default_factory=list)
+
+    def __post_init__(self) -> None:
+        """
+        Fill all class properties to same length.
+
+        Default value is an empty dict.
+        """
+        max_len = len(max(asdict(self).values(), key=len))
+
+        if not self.labels:
+            self.labels = [{} for _ in range(max_len)]
+
+        if not self.awards:
+            self.awards = [{} for _ in range(max_len)]
+
+        if not self.notes:
+            self.notes = [{} for _ in range(max_len)]
+
+        if not self.note_awards:
+            self.note_awards = [{} for _ in range(max_len)]
+
+
+@dataclass
+class EventCandidateContainer:
     """
-    Represents a node in a provenance graph.
+    A container for API resources that represent gl2p resource events.
+    """
+    labels: List[List[APIResource]] = field(default_factory=list)
+    awards: List[List[APIResource]] = field(default_factory=list)
+    notes: List[List[APIResource]] = field(default_factory=list)
+    note_awards: List[List[APIResource]] = field(default_factory=list)
+
+    def __post_init__(self) -> None:
+        """
+        Fill all class properties to same length.
+
+        Default value is an empty list.
+        """
+        max_len = len(max(asdict(self).values(), key=len))
+
+        if not self.labels:
+            self.labels = [[] for _ in range(max_len)]
+
+        if not self.awards:
+            self.awards = [[] for _ in range(max_len)]
+
+        if not self.notes:
+            self.notes = [[] for _ in range(max_len)]
+
+        if not self.note_awards:
+            self.note_awards = [[] for _ in range(max_len)]
+
+    def zip(self, iterable: Iterable[APIResource]) -> Iterable[Tuple[APIResource, EventCandidates]]:
+        """
+        Yield items from iterable zipped with items from container.
+        """
+        for item, *candidates in zip(iterable, self.labels, self.awards, self.notes, self.note_awards):
+            yield item, EventCandidates(*candidates)
+
+
+@dataclass
+class GL2PEvent:
+    """
+    Internal event representation.
     """
     id: str
-    label: Dict[str, str] = {}
+    initiator: str
+    label: Dict[str, Any]
+    created_at: str
 
 
-class PROVActivity(NamedTuple):
+class Activity(NamedTuple):
     """
-    Represents an activity node in a provenance graph.
+    Represent a PROV activity.
     """
     id: str
     start: datetime.datetime
     end: datetime.datetime
-    label: Dict[str, str] = {}
+    label: Dict[str, str]
+
+
+class Agent(NamedTuple):
+    """
+    Represent a PROV agent.
+    """
+    id: str
+    label: Dict[str, str]
+
+
+class Entity(NamedTuple):
+    """
+    Represent a PROV entity.
+    """
+    id: str
+    label: Dict[str, str]
 
 
 class Addition(NamedTuple):
     """
-    Represents a file addition for a commit activity.
+    Represent the addition of a new file.
     """
-    file: PROVNode
-    file_v: PROVNode
+    file: Entity
+    file_v: Entity
 
 
 class Modification(NamedTuple):
     """
-    Represents a file modification for a commit activity.
+    Represent the modification of a file.
     """
-    file: PROVNode
-    file_v: PROVNode
-    file_v_1: List[PROVNode]
+    file: Entity
+    file_v: Entity
+    file_v_1: List[Entity]
 
 
 class Deletion(NamedTuple):
     """
-    Represents a file deletion for a commit activity.
+    Represent the deletion of a file.
     """
-    file: PROVNode
-    file_v: PROVNode
+    file: Entity
+    file_v: Entity
 
 
-class Commit(NamedTuple):
+# PROV-DM Groupings
+class CommitResource(NamedTuple):
     """
-    Represents a commit.
+    Represent a git commit.
     """
-    author: PROVNode
-    committer: PROVNode
-    commit: PROVNode
-    parents: PROVNode
-    files: List[Union[Addition, Modification, Deletion]]
+    author: Agent
+    committer: Agent
+    commit: Activity
+    parents: List[Activity]
+    changes: List[Union[Addition, Deletion, Modification]]
 
 
-class CommitResourceCreation(NamedTuple):
+class Creation(NamedTuple):
     """
-    Represents the creation of a commit resource.
+    Represent the creation of a resource.
+    """
+    creator: Agent
+    creation: Activity
+    resource: Entity
+    resource_version: Entity
 
-    Special case of resource creation.
-    Link of commit model and commit resource model.
+
+class CommitCreation(NamedTuple):
     """
-    committer: PROVNode
-    commit: PROVNode
-    resource_creation: PROVNode
-    resource: PROVNode
-    resource_v: PROVNode
+    Represent the creation of a commit resource. Special case of Creation.
+    """
+    committer: Agent
+    commit: Activity
+    creation: Activity
+    resource: Entity
+    resource_version: Entity
 
 
 class Event(NamedTuple):
     """
-    Represents a resource event.
+    Represent an event occuring on a resource.
     """
-    initiator: PROVNode
-    event: PROVNode
-    previous_event: PROVNode
-    resource: PROVNode
-    resource_v: PROVNode
-    resource_v_1: PROVNode
+    initiator: Agent
+    event: Activity
+    previous_event: Activity
+    resource: Entity
+    resource_version: Entity
+    previous_resource_version: Entity
 
 
 class Resource(NamedTuple):
     """
-    Represents a resource.
+    Represent the lifecycle of a resource. From it's creation,
+    to a list of events occuring on the resource.
     """
-    creation: CommitResourceCreation
+    creation: Union[Creation, CommitCreation]
     events: List[Event]
