46569a5876ba5154846bdddc0a8eac5b6e5fbd01
Claas de Boer <cdboer@cdboer.de>
2020-02-12 20:21:48 +0100

    Rewrite gl2p/history.py
    Contents:
    - Added type hints
    - Removed subclassing of dictionary for NameTables
    - Added docstrings
    
    See also: #25, #26

--- gl2p/history.py
+++ gl2p/history.py
@@ -1,193 +1,201 @@
-# Copyright (c) 2019 German Aerospace Center (DLR/SC).
-# All rights reserved.
-#
-# This file is part of gitlab2prov.
-# gitlab2prov is licensed under the terms of the MIT License.
-# SPDX short Identifier: MIT
-#
-# You may obtain a copy of the License at:
-# https://opensource.org/licenses/MIT
-#
-# A command line tool to extract provenance data (PROV W3C)
-# from GitLab hosted repositories aswell as
-# to store the extracted data in a Neo4J database.
-#
-# code-author: Claas de Boer <claas.deboer@dlr.de>
+"""Compute the file name history from the commit tree."""
 
 
 from __future__ import annotations
 
-from collections import UserDict, defaultdict, deque
+import collections
 from copy import deepcopy
+from typing import Dict, List, Any, Set, Tuple
 from dataclasses import dataclass, field
-from functools import reduce
-from typing import Any, Dict, Iterator, List, Set, Union
+
+
+Diff = List[Dict[str, Any]]
+Commit = Dict[str, Any]
 
 
 @dataclass
-class FileNameRegister:
-    """
-    Track of file naming changes.
-
-    Builds a mappings of file names tot their original names for each commit in the commit tree.
-    """
-    commits: Union[Dict[str, Any], List[Any]]
-    diffs: Union[Dict[str, Any], List[Any]]
-    __register: Dict[str, NameTable] = field(default_factory=dict)
-
-    def __post_init__(self):
-        """
-        Transform class arguments into mappings and compute the register.
-        """
-        cs = {commit.get("id"): commit for commit in self.commits}
-        dfs = {
-            commit.get("id"): diff for commit, diff in zip(self.commits, self.diffs)
-        }
-
-        self.commits = cs
-        self.diffs = dfs
-        self.__register = self.compute_nametables()
+class FileNameHistory:
+    """Compute file name history for each point in the commit tree."""
+    history: Dict[str, NameTable] = field(default_factory=dict)
 
     def get(self, sha: str, file_name: str) -> str:
         """
-        Given a file name and a commit sha, return the original
-        name of the file according to the computed mapping of commit sha.
-
-        Returns:
-            str: The original file name of *file_name* if found else file_name
+        Return name that *file_name* is mapped to in NameTable of commit *sha*.
         """
-        if sha in self.__register:
-            nt = self.__register.get(sha, NameTable())
-            name = nt.get(file_name, file_name)
+        return self.history.get(sha, NameTable()).get(file_name, file_name)
 
-            return name
-
-        return file_name
-
-    def compute_nametables(self):
-        # TODO: better description of what actually happens here.
+    def compute(self, commits: List[Commit], diffs: List[Diff]) -> None:
         """
-        Compute mapping for each point in the commit tree.
+        Create NameTables for each commit by walking the commit tree.
 
-        Walk commit tree by BFS.
+        TODO: Description
         """
-        tree = self.commit_tree()
-        queue = deque(self.orphans())
+        clookup, dlookup = self.lookups(commits, diffs)
+        tree = self.commit_tree(commits)
+        orphans = self.orphans(commits)
 
-        register = {}
+        q = collections.deque(orphans)
 
-        while queue:
-            sha = queue.popleft()
-            diff = self.diffs.get(sha)
-            commit = self.commits.get(sha)
+        while q:
+            sha = q.popleft()
+            diff = dlookup[sha]
+            commit = clookup[sha]
 
-            if sha in register:
-                continue
+            if self.parents_cached(commit):
+                # skip if commit already has a computed NameTable
+                if sha in self.history:
+                    continue
 
-            if all(self.parents_cached(commit, register)):
-                parents = commit.get("parent_ids")
+                parents = commit["parent_ids"]
 
                 if not parents:
-                    register[sha] = NameTable.from_diff(diff)
+                    # init value
+                    # empty NameTable with diff applied
+                    self.history[sha] = NameTable.from_diff(diff)
                 else:
-                    nts = [register.get(parent) for parent in parents]
-                    nts = [nt.apply(diff) for nt in nts]
-                    register[sha] = reduce(NameTable.merge, nts)
+                    # NameTables of parent commits to current commit
+                    nts = [self.history[parent] for parent in parents]
 
-                queue.extend(tree.get(sha, []))
+                    # accumulator NameTable
+                    acc = nts[0].apply(diff)  # type: NameTable
+
+                    for nt in nts[1:]:
+                        # apply diff to NameTable before merging
+                        nt = nt.apply(diff)
+                        # merge into accumulator
+                        acc = acc.merge(nt)
+
+                    # store for current commit
+                    self.history[sha] = acc
+
+                # enqueue children of current commit
+                q.extend(tree.get(sha, []))
             else:
-                queue.append(sha)
+                # wait in queue iff parenting NameTables haven't been computed
+                q.append(sha)
 
-        return register
-
-    def commit_tree(self) -> Dict[str, Set[str]]:
+    def parents_cached(self, commit: Commit) -> bool:
         """
-        Compute the commit tree stemming from self.commits.
-
-        The tree is represented by a mapping of commit sha's
-        to sets of children sha's.
-
-        Returns:
-            Dict[str, set]: Mapping from commit sha's to sets of children sha's.
+        Return whether all parents of *commit* have an entry in self.history.
         """
-        tree = defaultdict(set)  # type: Dict[str, Set]
+        ps = commit["parent_ids"]
+        return all(p in self.history for p in ps)
 
-        for commit in self.commits.values():
-            commit, parents = commit.get("id"), commit.get("parent_ids", [])
+    def lookups(self, commits: List[Commit], diffs: List[Diff]) -> Tuple[Dict[str, Commit], Dict[str, Diff]]:
+        """
+        Return mapping of commit ids to commits and commit ids to diffs.
+        """
+        if len(commits) != len(diffs):
+            raise ValueError(
+                f"{len(commits)} Commits. {len(diffs)} Diffs.\
+                Expected same amount of Commits and Diffs."
+            )
 
-            for parent in parents:
-                tree[parent].add(commit)
+        cl, dl = {}, {}
+
+        for c, d in zip(commits, diffs):
+            cl[c["id"]] = c
+            dl[c["id"]] = d
+
+        return cl, dl
+
+    def commit_tree(self, commits: List[Commit]) -> Dict[str, Set[str]]:
+        """
+        Return commit tree build from commits.
+
+        Tree represented as mapping of commit id's to set's of children
+        commit id's.
+        """
+        tree = collections.defaultdict(set)  # type: Dict[str, Set[str]]
+
+        for c in commits:
+            c_id = c["id"]  # type: str
+            parent_ids = c["parent_ids"]  # type: str
+
+            for p_id in parent_ids:
+                tree[p_id].add(c_id)
 
         return tree
 
-    def orphans(self) -> Iterator[str]:
-        """
-        Generator for commits that don't have parents.
-
-        Yields:
-            str: The sha of a commit
-        """
-        for commit in self.commits.values():
-            if not commit.get("parent_ids"):
-                yield commit.get("id")
-
-    def parents_cached(self, commit: Dict[str, Any], register: Dict[str, Any]) -> Iterator[bool]:
-        """
-        Generator yielding whether parents of commit have a cached nametable.
-
-        Parameters:
-            commit: Commit for which to check whether parents have a nametable.
-        Yields:
-            bool: Whether parent has an entry in nametable cache.
-        """
-        for parent in commit.get("parent_ids", []):
-            yield (parent in register)
+    def orphans(self, commits: List[Commit]) -> List[str]:
+        """Return list of commits that do not have parents."""
+        return [c["id"] for c in commits if not c["parent_ids"]]
 
 
-class NameTable(UserDict):
+@dataclass
+class NameTable:
     """
-    A mapping of file names to their original names for a single commit.
-
-    Generate NameTable for commit x by applying diff of x to NameTable of commit x-1.
+    A mapping of filenames to the names that they had when they got added.
+    Suffices for just one commit n.
     """
 
-    @staticmethod
-    def merge(nt1: NameTable, nt2: NameTable) -> NameTable:
-        """
-        Merge two nametables nt1, nt2.
-
-        Returns:
-            NameTable: A name table containing all entries
-             from nt1 updated with entries from nt2.
-        """
-        copy = deepcopy(nt1.data)
-        copy.update(nt2.data)
-
-        return NameTable(copy)
+    data: Dict[str, str] = field(default_factory=dict)
 
     @classmethod
-    def from_diff(cls, diff) -> NameTable:
+    def from_diff(cls, diff: Diff) -> NameTable:
         """
-        Instantiate an empty NameTable and apply diff to it.
+        Return NameTable instance created from *diff*.
+
+        Create an empty NameTable and apply *diff* to it.
         """
         return cls().apply(diff)
 
-    def apply(self, diff: List[Dict[str, Any]]) -> NameTable:
+    def get(self, name: str, default: str = "") -> str:
         """
-        Apply a diff to the current NameTable.
+        Return NameTable entry for key name.
+
+        Return default if key is not in self.data.
+        """
+        return self.data.get(name, default)
+
+    def merge(self, other: NameTable) -> NameTable:
+        """
+        Return NameTable resulting from merge of *self* with *other*.
+
+        Merge two NameTable's (*self* and *other*) by updating the
+        entries of *self* with the entries of *other*.
+        """
+        if not isinstance(other, NameTable):
+            raise TypeError(f"Expected type NameTable, got {type(other)}")
+
+        copy = deepcopy(self.data)
+        copy.update(other.data)
+
+        return NameTable(copy)
+
+    def apply(self, diff: Diff) -> NameTable:
+        """
+        Return copy of *self* with diff applied.
+
+        We can compute the NameTable NT of commit n+1 given *diff* and
+        *self* as follows:
+
+        for each entry ...
+            ... let NT be a copy of NameTable *self*
+            ... let NEW be the new file path of f
+            ... let OLD be the old file path of f
+
+            ,if
+                ... entry denotes that f has been added,
+                    - add NT entry NEW -> OLD
+
+                ... NEW != OLD and OLD has an entry in NT,
+                    - add NT entry NEW -> NT[OLD]
+
+                ... NEW still not in NT,
+                    - add NT entry NEW -> OLD
         """
         copy = deepcopy(self.data)
-        for entry in diff:
-            new, old = entry.get("new_path"), entry.get("old_path")
 
-            if entry.get("new_file"):
+        for entry in diff:
+            new = entry["new_path"]  # type: str
+            old = entry["old_path"]  # type: str
+
+            if entry["new_file"]:
                 copy[new] = old
 
-            if new != old:
-                root = copy.get(old)
-                if not root:
-                    continue
-                copy[new] = root
+            if new != old and old in copy:
+                copy[new] = copy[old]
 
             if new not in copy:
                 copy[new] = old
