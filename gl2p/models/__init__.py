9c0b4d5d0bba7651b4e7891585487d2ac6dba29b
Claas de Boer <cdboer@cdboer.de>
2020-02-12 20:28:45 +0100

    Refactor and add resource model
    Adds model implementation for MergeRequests and Issues.
    Refactor existing code.
    
    Contents:
    - Add type hints
    - Add docstrings
    - Add resource model
    
    See also: #25, #26

--- gl2p/models/__init__.py
+++ gl2p/models/__init__.py
@@ -16,120 +16,148 @@
 
 
 from dataclasses import InitVar, dataclass, field
-from typing import Any, Optional, Set, List, Union
-
-from gl2p.objects import (Addition, Deletion,
-                          Modification, Resource, Commit)
-from prov.model import ProvBundle, ProvDocument
+from typing import Any, Set, Union, Tuple, List
+from prov.model import ProvDocument
+from gl2p.utils.objects import (Addition, Deletion, Creation, CommitCreation, Event,
+                                Modification, Resource, CommitResource)
 
 
 @dataclass
 class Model:
     """
-    Model implementation base class for models in ../models.
+    Abstract base class for a model.
     """
     project_id: InitVar[str]
-    doc: Optional[ProvDocument] = None
-    bundle: Optional[ProvBundle] = None
 
-    def __post_init__(self, project_id: str):
+    def __post_init__(self, project_id: str) -> None:
+        """
+        Create a new prov document to store the model in.
+
+        Decode url encoded project id.
+        """
         self.project_id = project_id.replace("%2F", "-")
-        self.doc = ProvDocument()
-        self.doc.set_default_namespace("gl2p:")
-        self.bundle = self.doc.bundle(self.project_id)
+        self.__doc = ProvDocument()
+        self.__doc.set_default_namespace("gl2p:")
+        self._bundle = self.__doc.bundle(self.project_id)
 
-    def push(self, resource: Any):
+    def push(self, resource: Union[Resource, CommitResource]) -> None:
+        """
+        Abstract method to push a resource/commit into the model.
+        """
         raise NotImplementedError
 
     def document(self) -> ProvDocument:
-        return self.doc
+        """
+        Return the model document.
+        """
+        return self.__doc.unified()
 
 
 @dataclass
-class Commit(Model):
+class CommitModel(Model):
     """
     Model implementation for commit model.
     """
-    relation_store: Set = field(default_factory=set)
+    relation_store: Set[Tuple[str, str]] = field(default_factory=set)
 
-    def push(self, resource: Commit) -> None:
+    def push(self, resource: Union[Resource, CommitResource]) -> None:
         """
-        Push resource into model document.
+        Add a resource to the model.
         """
-        bundle = self.bundle
+        # TODO: fix
+        if isinstance(resource, Resource):
+            raise TypeError
+
         _, _, commit, parents, files = resource
 
-        self.add_commit(bundle, resource)
-
+        self._add_commit(resource)
         for parent in parents:
-            self.add_parent(bundle, resource, parent)
-
+            self._add_parent(resource, parent)
         for f in files:
-            self.add_file(bundle, resource, f)
+            self._add_file(resource, f)
 
-        return
-
-    def add_commit(self, bundle: ProvBundle, resource: Commit) -> None:
+    def _add_commit(self, resource: CommitResource) -> None:
         """
         Add commit nodes and relations of *resource* to *bundle*.
         """
         author, committer, commit, *_ = resource
-        bundle.agent(*author)
-        bundle.agent(*committer)
-        bundle.activity(*commit)
-        bundle.wasAssociatedWith(commit.id, author.id)
-        bundle.wasAssociatedWith(commit.id, committer.id)
-        return
+        self._bundle.agent(*author)
+        self._bundle.agent(*committer)
+        self._bundle.activity(*commit)
+        self._bundle.wasAssociatedWith(commit.id, author.id)
+        self._bundle.wasAssociatedWith(commit.id, committer.id)
 
-    def add_parent(self, bundle: ProvBundle, resource: Commit, parent: Any) -> None:
+    def _add_parent(self, resource: CommitResource, parent: Any) -> None:
         """
         Add parent nodes and relations of *parent* to *bundle*.
         """
         _, _, commit, *_ = resource
-        bundle.activity(*parent)
-        bundle.wasInformedBy(commit.id, parent.id)
-        return
+        self._bundle.activity(*parent)
+        self._bundle.wasInformedBy(commit.id, parent.id)
 
-    def add_file(self, bundle: ProvBundle, resource: Commit, f: List[Union[Addition, Modification, Deletion]]) -> None:
+    def _add_file(self, resource: CommitResource, file_: Union[Addition, Modification, Deletion]) -> None:
         """
         Add file relations and nodes stemming from *f* to *bundle*.
         """
         author, committer, commit, *_ = resource
 
-        if isinstance(f, Addition):
-            f, fv = f
-            bundle.entity(*f)
-            bundle.entity(*fv)
-            bundle.wasGeneratedBy(f.id, commit.id)
-            bundle.wasGeneratedBy(fv.id, commit.id)
-            bundle.wasAttributedTo(f.id, author.id)
-            bundle.wasAttributedTo(fv.id, author.id)
-            if self.unique_specialization_of(fv.id, f.id):
-                bundle.specializationOf(fv.id, f.id)
+        if isinstance(file_, Addition):
+            self._add_addition(resource, file_)
 
-        if isinstance(f, Modification):
-            f, fv, fv_1s = f
-            bundle.entity(*f)
-            bundle.entity(*fv)
-            bundle.wasAttributedTo(fv.id, commit.id)
-            bundle.wasGeneratedBy(fv.id, commit.id)
-            if self.unique_specialization_of(fv.id, f.id):
-                bundle.specializationOf(fv.id, f.id)
-            for fv_1 in fv_1s:
-                bundle.entity(*fv_1)
-                bundle.used(commit.id, fv_1.id)
-                bundle.wasDerivedFrom(fv.id, fv_1.id)
-                if self.unique_specialization_of(fv_1.id, f.id):
-                    bundle.specializationOf(fv_1.id, f.id)
+        if isinstance(file_, Modification):
+            self._add_modification(resource, file_)
 
-        if isinstance(f, Deletion):
-            f, fv = f
-            bundle.entity(*f)
-            bundle.entity(*fv)
-            if self.unique_specialization_of(fv.id, f.id):
-                bundle.specializationOf(fv.id, f.id)
-            bundle.wasInvalidatedBy(fv.id, commit.id)
-        return
+        if isinstance(file_, Deletion):
+            self._add_deletion(resource, file_)
+
+    def _add_addition(self, resource: CommitResource, addition: Addition) -> None:
+        """
+        Add file change addition relations and nodes to the model.
+        """
+        author, committer, commit, *_ = resource
+
+        f, fv = addition
+        self._bundle.entity(*f)
+        self._bundle.entity(*fv)
+        self._bundle.wasGeneratedBy(f.id, commit.id)
+        self._bundle.wasGeneratedBy(fv.id, commit.id)
+        self._bundle.wasAttributedTo(f.id, author.id)
+        self._bundle.wasAttributedTo(fv.id, author.id)
+        if self.unique_specialization_of(fv.id, f.id):
+            self._bundle.specializationOf(fv.id, f.id)
+
+    def _add_modification(self, resource: CommitResource, modification: Modification) -> None:
+        """
+        Add file change modification relations and nodes to the model.
+        """
+        author, committer, commit, *_ = resource
+
+        f, fv, fv_1s = modification
+        self._bundle.entity(*f)
+        self._bundle.entity(*fv)
+        self._bundle.wasAttributedTo(fv.id, author.id)
+        self._bundle.wasGeneratedBy(fv.id, commit.id)
+        if self.unique_specialization_of(fv.id, f.id):
+            self._bundle.specializationOf(fv.id, f.id)
+        for fv_1 in fv_1s:
+            self._bundle.entity(*fv_1)
+            self._bundle.used(commit.id, fv_1.id)
+            self._bundle.wasDerivedFrom(fv.id, fv_1.id)
+            if self.unique_specialization_of(fv_1.id, f.id):
+                self._bundle.specializationOf(fv_1.id, f.id)
+
+    def _add_deletion(self, resource: CommitResource, deletion: Deletion) -> None:
+        """
+        Add file change deletion relations and nodes to the model.
+        """
+        author, committer, commit, *_ = resource
+
+        f, fv = deletion
+        self._bundle.entity(*f)
+        self._bundle.entity(*fv)
+        if self.unique_specialization_of(fv.id, f.id):
+            self._bundle.specializationOf(fv.id, f.id)
+        self._bundle.wasInvalidatedBy(fv.id, commit.id)
 
     def unique_specialization_of(self, start: str, target: str) -> bool:
         """
@@ -145,55 +173,94 @@ class Commit(Model):
 
 
 @dataclass
-class CommitResource(Model):
+class ResourceModel(Model):
     """
-    Model implementation for commit resource model.
+    Resource Model.
     """
 
-    def push(self, resource: Resource) -> None:
+    def push(self, resource: Union[Resource, CommitResource]) -> None:
         """
         Push resource into model document.
         """
-        bundle = self.bundle
+        # TODO: fix
+        if isinstance(resource, CommitResource):
+            raise TypeError
 
-        # creation
-        creation, _ = resource
+        creation, events = resource
+        self._add_creation(creation)
+        self._add_event_chain(events)
 
-        committer, commit, rcreation, r, rv = creation
+    def _add_creation(self, creation_bundle: Union[Creation, CommitCreation]) -> None:
+        """
+        Resource creation.
+        """
+        # TODO: fix
+        if isinstance(creation_bundle, CommitCreation):
+            raise TypeError
+        creator, creation, r, rv = creation_bundle
 
-        bundle.activity(*commit)
-        bundle.activity(*rcreation)
-        bundle.agent(*committer)
-        bundle.entity(*r)
-        bundle.entity(*rv)
+        self._bundle.activity(*creation)
+        self._bundle.entity(*r)
+        self._bundle.entity(*rv)
+        self._bundle.agent(*creator)
 
-        bundle.wasAssociatedWith(commit.id, committer.id)
-        bundle.wasAssociatedWith(rcreation.id, committer.id)
-        bundle.wasAttributedTo(r.id, committer.id)
-        bundle.wasInformedBy(rcreation.id, commit.id)
-        bundle.wasAttributedTo(rv.id, committer.id)
-        bundle.wasGeneratedBy(r.id, rcreation.id)
-        bundle.wasGeneratedBy(rv.id, rcreation.id)
-        bundle.specializationOf(rv.id, r.id)
+        self._bundle.wasAssociatedWith(creation.id, creator.id)
+        self._bundle.wasAttributedTo(r.id, creator.id)
+        self._bundle.wasAttributedTo(rv.id, creator.id)
+        self._bundle.wasGeneratedBy(r.id, creation.id)
+        self._bundle.wasGeneratedBy(rv.id, creation.id)
+        self._bundle.specializationOf(rv.id, r.id)
 
-        # event chain
-        _, events = resource
-
+    def _add_event_chain(self, events: List[Event]) -> None:
+        """
+        Resource event chain.
+        """
         for event in events:
             user, e, eprev, r, rv, rv_1 = event
 
-            bundle.entity(*r)
-            bundle.entity(*rv)
-            bundle.entity(*rv_1)
-            bundle.activity(*e)
-            bundle.activity(*eprev)
-            bundle.agent(*user)
+            self._bundle.entity(*r)
+            self._bundle.entity(*rv)
+            self._bundle.entity(*rv_1)
+            self._bundle.activity(*e)
+            self._bundle.activity(*eprev)
+            self._bundle.agent(*user)
 
-            bundle.specializationOf(rv.id, r.id)
-            bundle.used(e.id, rv_1.id)
-            bundle.wasInformedBy(e.id, eprev.id)
-            bundle.wasAssociatedWith(e.id, user.id)
-            bundle.wasDerivedFrom(rv.id, rv_1.id)
-            bundle.wasAttributedTo(rv.id, user.id)
-            bundle.wasGeneratedBy(rv.id, e.id)
-        return
+            self._bundle.specializationOf(rv.id, r.id)
+            self._bundle.used(e.id, rv_1.id)
+            self._bundle.wasInformedBy(e.id, eprev.id)
+            self._bundle.wasAssociatedWith(e.id, user.id)
+            self._bundle.wasDerivedFrom(rv.id, rv_1.id)
+            self._bundle.wasAttributedTo(rv.id, user.id)
+            self._bundle.wasGeneratedBy(rv.id, e.id)
+
+
+@dataclass
+class CommitResourceModel(ResourceModel):
+    """
+    Commit resource model.
+    """
+
+    def _add_creation(self, creation_bundle: Union[Creation, CommitCreation]) -> None:
+        """
+        Commit resource creation.
+        """
+        # TODO: fix
+        if isinstance(creation_bundle, Creation):
+            raise TypeError
+
+        committer, commit, creation, r, rv = creation_bundle
+
+        self._bundle.activity(*commit)
+        self._bundle.activity(*creation)
+        self._bundle.agent(*committer)
+        self._bundle.entity(*r)
+        self._bundle.entity(*rv)
+
+        self._bundle.wasAssociatedWith(commit.id, committer.id)
+        self._bundle.wasAssociatedWith(creation.id, committer.id)
+        self._bundle.wasAttributedTo(r.id, committer.id)
+        self._bundle.wasInformedBy(creation.id, commit.id)
+        self._bundle.wasAttributedTo(rv.id, committer.id)
+        self._bundle.wasGeneratedBy(r.id, creation.id)
+        self._bundle.wasGeneratedBy(rv.id, creation.id)
+        self._bundle.specializationOf(rv.id, r.id)
