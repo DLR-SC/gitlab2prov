58df3fec7e5af37ce3c9acacabf643fb00410859
Claas de Boer <cdboer@cdboer.de>
2020-02-14 20:21:55 +0100

    Add all available config options as cl flags
    All configuration options can now be set by command line flags.
    Command line flags take precedence over values read from the config file.
    If a necessary configuration option does not have a value neither from a
    flag nor from the config file a ConfigurationException is raised.
    Command line flags do not change values of the config file.
    
    Add GitHub project link to the epilogue of the usage message.
    
    Contents
    - add flag to change config file path
    - add flag to specify a project url
    - add flag to specify an api access token
    - add flag to specify a rate limit for the api client
    - add flag to change output format of the provenance document
    - add flag to enable/disable neo4j storage
    - add flag to specify neo4j host
    - add flag to specify neo4j boltport
    - add flag to specify neo4j username
    - add flag to specify neo4j password
    
    See also: #27

--- gl2p/config.py
+++ gl2p/config.py
@@ -16,48 +16,118 @@
 
 
 import os
-from configparser import ConfigParser
-
-CONFIG_PATH = "./config/config.ini"
-
-VALID_CONFIG = {
-        "GITLAB":
-        [
-            "token",
-            "rate",
-            "project"
-        ],
-        "NEO4J":
-        [
-            "host",
-            "user",
-            "password"
-        ]
-    }
+import argparse
+import configparser
+from typing import Dict, Any
 
 
 class ConfigurationException(Exception):
     pass
 
 
-if not os.path.exists(CONFIG_PATH):
-    raise ConfigurationException(f"Missing CONFIG file: {CONFIG_PATH}")
+def get_config() -> argparse.Namespace:
+    """
+    Return configuration Namespace.
+    """
+    # dictionary of arguments
+    args = vars(parse_args())
 
-config = ConfigParser()
-config.read(CONFIG_PATH)
+    if not os.path.exists(args["config_file"]):
+        raise ConfigurationException(f"Missing config file: {args['config_file']}")
 
-CONFIG = config
-TOKEN = config.get("GITLAB", "token")
-PROJECT = config.get("GITLAB", "project")
-RATE_LIMIT = int(config.get("GITLAB", "rate"))
-NEO4J_HOST = config.get("NEO4J", "host")
-NEO4J_USER = config.get("NEO4J", "user")
-NEO4J_PASSWORD = config.get("NEO4J", "password")
-NEO4J_BOLT_PORT = config.get("NEO4J", "boltport")
+    config = configparser.ConfigParser()
+    config.read(args["config_file"])
 
-for section in VALID_CONFIG.keys():
-    if section not in CONFIG.sections():
-        raise ConfigurationException(f"Section {section} is missing!")
-    for param in VALID_CONFIG[section]:
-        if param not in CONFIG[section]:
-            raise ConfigurationException(f"Parameter {param} in section {section} is missing!")
+    del args["config_file"]
+
+    # path missing values with once from config file
+    args = patch_args(args, config)
+
+    # convert to int
+    args["rate_limit"] = int(args["rate_limit"])
+
+    # convert back to namespace
+    return argparse.Namespace(**args)
+
+
+def parse_args() -> argparse.Namespace:
+    """
+    Parse command line arguments.
+    """
+    parser = argparse.ArgumentParser(
+        description="Extract provenance information from GitLab projects.",
+        epilog="Consider visiting GitLab2PROV on GitHub: https://github.com/DLR-SC/gitlab2prov"
+    )
+
+    basic = parser.add_argument_group("BASIC CONFIG")
+    basic.add_argument("-p", "--project-url", help="gitlab project url", metavar="URL")
+    basic.add_argument("-t", "--token", help="gitlab api access token", metavar="TOKEN")
+    basic.add_argument(
+        "-r", "--rate-limit",
+        help="api client rate limit (in req/s)",
+        metavar="LIMIT",
+        type=int
+    )
+    basic.add_argument(
+        "-c", "--config-file",
+        help="config file path",
+        default="config/config.ini",
+        metavar="CONFIG"
+    )
+    basic.add_argument(
+        "-f", "--format",
+        help="provenance output format",
+        choices=["provn", "json", "rdf", "xml"]
+    )
+
+    neo4j = parser.add_argument_group("NEO4J CONFIG")
+    neo4j.add_argument("--neo4j", help="enable neo4j storage", action="store_true")
+    neo4j.add_argument("--neo4j-user", help="neo4j username", metavar="USERNAME")
+    neo4j.add_argument("--neo4j-password", help="neo4j password", metavar="PASSWORD")
+    neo4j.add_argument("--neo4j-host", help="neo4j host", metavar="HOST")
+    neo4j.add_argument("--neo4j-boltport", help="neo4j bolt protocol port", metavar="PORT")
+
+    return parser.parse_args()
+
+
+def patch_args(args: Dict[str, Any], config: configparser.ConfigParser) -> Dict[str, Any]:
+    """
+    Fill missing values of args with values from config file.
+
+    Command line arguments, if given, take precedence.
+    Sets default values for optional flags such as --format and --rate-limit.
+    Raise exception if required values are missing.
+    """
+    required = ["project_url", "token", "neo4j_user", "neo4j_password", "neo4j_host", "neo4j_boltport"]
+
+    if not args["neo4j"]:
+        to_be_removed = ["neo4j_user", "neo4j_password", "neo4j_host", "neo4j_boltport"]
+        for key in to_be_removed:
+            required.remove(key)
+            del args[key]
+
+    # refill keys from config if empty
+    for key in args.keys():
+
+        if key == "neo4j":
+            # skip neo4j flag
+            continue
+
+        if not args[key]:
+
+            if key in required and key not in config["GITLAB2PROV"]:
+                # key not in config file
+                raise ConfigurationException(f"Config argument {key} is necessary, though no value has been given.")
+
+            if key not in required:
+                # default values for optional flags
+                if key == "format":
+                    args[key] = config.get("GITLAB2PROV", key, fallback="provn")
+                    continue
+                if key == "rate_limit":
+                    args[key] = config.get("GITLAB2PROV", key, fallback=10)
+                    continue
+
+            args[key] = config.get("GITLAB2PROV", key)
+
+    return args
