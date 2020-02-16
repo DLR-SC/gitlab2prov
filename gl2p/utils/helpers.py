b2a57a201a506959f9d744e7689cb6b9e1972175
Claas de Boer <cdboer@cdboer.de>
2020-02-16 21:41:04 +0100

    Refactoring and behaviour correction of helpers.py
    Refactor doc strings, rename parameters and raise appropriate exceptions
    when encountering nonsensical parameter values. Exceptions are expected
    in test cases, the changes satisfy the specified behaviour.
    
    See also: #25

--- gl2p/utils/helpers.py
+++ gl2p/utils/helpers.py
@@ -28,45 +28,68 @@ def ptime(string: str) -> datetime.datetime:
     """
     Parse datetimestring to datetime object.
     """
+    if not isinstance(string, str):
+        raise TypeError(f"Parameter string: expected type str, got type {type(string)}.")
+
     fmt = "%Y-%m-%dT%H:%M:%S.%f%z"
-    date = datetime.datetime.strptime(string, fmt)
-    return date
+    return datetime.datetime.strptime(string, fmt)
 
 
 def by_date(event: GL2PEvent) -> datetime.datetime:
     """
-    Parse value of key 'created_at' of resource to datetime object.
+    Parse value of attribute 'created_at' of resource to datetime object.
     """
+    if not isinstance(event, GL2PEvent):
+        raise TypeError(f"Parameter event: Expected type GL2PEvent, got type {type(event)}.")
+
     return ptime(event.created_at)
 
 
-def chunks(lst: List[Any], chunk_size: int) -> Iterator[List[Any]]:
+def chunks(L: List[Any], chunk_size: int) -> Iterator[List[Any]]:
     """
-    Generator for n-sized chunks of list l.
+    Generator for n-sized chunks of list *L*.
     """
-    for i in range(0, len(lst), chunk_size):
-        yield lst[i: i+chunk_size]
+    if chunk_size is None or chunk_size == 0:
+        raise ValueError(f"Parameter chunk_size: Unexpected value {chunk_size}.")
+
+    if not isinstance(L, list):
+        raise TypeError(f"Parameter L: Expected type list, got type {type(L)}.")
+
+    if not isinstance(chunk_size, int):
+        raise TypeError(f"Parameter chunk_size: Expected type int, got type {type(chunk_size)}.")
+
+    for i in range(0, len(L), chunk_size):
+        yield L[i: i+chunk_size]
 
 
 def url_encoded_path(url: str) -> str:
     """
     Extract project path from url and replace "/" by "%2F".
     """
+    if not isinstance(url, str):
+        raise TypeError(f"Parameter url: Expected type str, got type {type(url)}.")
+
     path = urllib.parse.urlparse(url).path
+    
+    if not path:
+        raise ValueError(f"Could not parse path from {url}.")
 
     if path.endswith("/"):
         path = path[:-1]
 
     if not path:
-        return ""
+        raise ValueError(f"Empty path parsed from {url}.")
 
     return path[1:].replace("/", "%2F")
 
 
 def qname(string: str) -> str:
     """
-    Return uuid5 of *string* in string representation.
-
-    Used to create unique identifiers.
+    Return string representation of uuid5 of *string*.
+    
+    Creates unique identifiers.
     """
+    if not isinstance(string, str):
+        raise TypeError(f"Parameter string: Expected type str, got type {type(string)}.")
+
     return str(uuid.uuid5(uuid.NAMESPACE_DNS, string))
