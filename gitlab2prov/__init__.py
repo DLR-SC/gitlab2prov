c3bbf1f216fa94591479c0d474c5a8b74bc534f9
Andreas Schreiber <andreas.schreiber@dlr.de>
2021-04-08 11:15:37 +0200

    Add module metadata.

--- gitlab2prov/__init__.py
+++ gitlab2prov/__init__.py
@@ -1,3 +1,11 @@
+"""Extract provenance from GitLab projects."""
+
+__author__ = "Claas de Boer, Andreas Schreiber"
+__copyright__ = "Copyright 2020, German Aerospace Center (DLR) and individual contributors"
+__license__ = "MIT"
+__version__ = "0.3"
+__status__ = "Development"
+
 import re
 import asyncio
 from collections import namedtuple
