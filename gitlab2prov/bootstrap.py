055240d57fdf6f8b928eb54b68c71dbb506cb49b
cdboer <cdboer@cdboer.de>
2022-02-13 21:52:53 +0100

    Create bootstrap.py

---
+++ gitlab2prov/bootstrap.py
@@ -0,0 +1,38 @@
+import inspect
+import logging
+
+from gitlab2prov.service_layer import handlers, messagebus, unit_of_work
+from gitlab2prov.adapters import miners
+
+
+log = logging.getLogger(__name__)
+
+
+def bootstrap(
+    uow: unit_of_work.AbstractUnitOfWork = unit_of_work.InMemoryUnitOfWork(),
+    git_miner: miners.AbstractGitMiner = miners.GitRepositoryMiner(),
+    gitlab_miner: miners.AbstractGitlabMiner = miners.GitlabProjectMiner(),
+):
+    dependencies = {
+        "uow": uow,
+        "git_miner": git_miner,
+        "gitlab_miner": gitlab_miner,
+    }
+    injected_handlers = {
+        command_type: [
+            inject_dependencies(handler, dependencies) for handler in handlers
+        ]
+        for command_type, handlers in handlers.HANDLERS.items()
+    }
+
+    return messagebus.MessageBus(uow, handlers=injected_handlers)
+
+
+def inject_dependencies(handler, dependencies):
+    params = inspect.signature(handler).parameters
+    dependencies = {
+        name: dependency for name, dependency in dependencies.items() if name in params
+    }
+    for name, dep in dependencies.items():
+        log.debug(f"inject dependency {dep} into handler {handler} as param {name}")
+    return lambda cmd: handler(cmd, **dependencies)
