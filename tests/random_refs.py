33f1fbca0b819783a3a7a3ca7b92841536a844fa
cdboer <cdboer@cdboer.de>
2022-02-16 16:27:41 +0100

    Add random user generator to random_refs.py

--- tests/random_refs.py
+++ tests/random_refs.py
@@ -4,3 +4,13 @@ import random
 
 def random_suffix():
     return uuid.uuid4().hex[:6]
+
+
+def random_user():
+    name = f"user-name-{random_suffix()}"
+    email = f"user-email-{random_suffix()}"
+    username = f"gitlab-user-name-{random_suffix()}"
+    id = f"gitlab-user-id-{random_suffix()}"
+    role = random.choice(list(vars(ProvRole).values()))
+    return objects.User(name, email, username, id, role)
+
