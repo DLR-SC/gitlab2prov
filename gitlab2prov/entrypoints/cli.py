619907e505f7108d68242e42dd3395e289073448
cdboer <cdboer@cdboer.de>
2022-03-27 15:01:06 +0200

    Update cli.py
    Make use of the new logging and profiling modules.
    Use the new config api to simplify the config reading process.
    This should make the entrypoint way easier to understand than in the previous version, hiding complexity and presenting only the parts that matter.

--- gitlab2prov/entrypoints/cli.py
+++ gitlab2prov/entrypoints/cli.py
@@ -1,32 +1,25 @@
-import tempfile
-
-from gitlab2prov.config import CONFIG, set_config
-if CONFIG is None:
-    CONFIG = set_config(entrypoint="cli")
-
 from gitlab2prov import bootstrap
+from gitlab2prov.config import read_cli
 from gitlab2prov.domain import commands
+from gitlab2prov.log import create_logger
+from gitlab2prov.profile import profiling
 
 
+config = read_cli()
 
 
-
-
-
+@profiling(enabled=config.profile)
 def main():
     bus = bootstrap.bootstrap()
-    token = CONFIG.token
 
-    for url in CONFIG.projects:
-        # create a temporary directory to clone
-        # the project git repo to
-        with tempfile.TemporaryDirectory() as tmpdir:
-            log.info(f"created {tmpdir=}")
-            cmd = commands.Init(url, token, tmpdir)
-            bus.handle(cmd)
+    if config.verbose:
+        create_logger()
 
-    # write serialized graph to stdout
-    cmd = commands.Serialize(CONFIG.fmt, CONFIG.pseudonymous, CONFIG.double_agents)
+    for url in config.project_urls:
+        cmd = commands.Fetch(url, config.token)
+        bus.handle(cmd)
+
+    cmd = commands.Serialize(config.format, config.pseudonymous, config.double_agents)
     bus.handle(cmd)
 
 
