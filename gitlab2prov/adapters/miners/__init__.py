f20e36fc160c01dfd7f92cce77cda18e03154458
cdboer <cdboer@cdboer.de>
2022-03-27 15:01:22 +0200

    Update __init__.py

--- gitlab2prov/adapters/miners/__init__.py
+++ gitlab2prov/adapters/miners/__init__.py
@@ -1,2 +1,2 @@
-from gitlab2prov.adapters.miners.git import AbstractGitMiner, GitRepositoryMiner
-from gitlab2prov.adapters.miners.gitlab import AbstractGitlabMiner, GitlabProjectMiner
+from gitlab2prov.adapters.miners.git import AbstractMiner, GitRepositoryMiner
+from gitlab2prov.adapters.miners.gitlab import AbstractMiner, GitlabProjectMiner
