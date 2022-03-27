229f2a7ee3b747cb7c7381fde65dea9a9d3dfe9c
cdboer <cdboer@cdboer.de>
2022-03-27 14:51:44 +0200

    Introduce seperate serialization function
    Updates the serialize handler to use the updated commands.Serialize properties.
    Make use of the new serialize function.
    Add ProvDocument type hint.

--- gitlab2prov/service_layer/handlers.py
+++ gitlab2prov/service_layer/handlers.py
@@ -1,4 +1,3 @@
-import sys
 import logging
 from urllib.parse import urlsplit
 from tempfile import TemporaryDirectory
@@ -6,6 +5,7 @@ from tempfile import TemporaryDirectory
 from git import Repo
 from gitlab import Gitlab
 from prov.dot import prov_to_dot
+from prov.model import ProvDocument
 
 from gitlab2prov.domain import commands
 from gitlab2prov.prov import operations
@@ -32,6 +32,11 @@ def clone_with_https_url(url: str, token: str) -> str:
     return f"https://gitlab.com:{token}@{split.netloc}/{project_slug(url)}"
 
 
+def serialize_graph(graph: ProvDocument, fmt: str):
+    if fmt == "dot":
+        return prov_to_dot(graph)
+    return graph.serialize(format=fmt)
+
 
 def mine_git(cmd: commands.Fetch, uow, git_miner) -> None:
     url = clone_with_https_url(cmd.project_url, cmd.token)
@@ -55,8 +60,9 @@ def mine_gitlab(cmd: commands.Fetch, uow, gitlab_miner) -> None:
         uow.commit()
 
 
-def serialize(cmd: commands.Serialize, uow):
+def serialize(cmd: commands.Serialize, uow) -> None:
     subgraphs = []
+
     for prov_model in model.MODELS:
         with uow:
             log.info(f"populate model {prov_model}")
@@ -66,16 +72,15 @@ def serialize(cmd: commands.Serialize, uow):
     graph = operations.combine(subgraphs)
     graph = operations.dedupe(graph)
 
+    # optional operations
     if cmd.uncover_double_agents:
         graph = operations.uncover_double_agents(graph, cmd.uncover_double_agents)
         graph = operations.dedupe(graph)
     if cmd.pseudonymize:
         graph = operations.pseudonymize(graph)
-    
-    if cmd.fmt == "dot":
-        print(prov_to_dot(graph), file=sys.stdout)
-    else:
-        graph.serialize(destination=sys.stdout, format=cmd.fmt)
+
+    # write to stdout
+    print(serialize_graph(graph, cmd.format))
 
 
 HANDLERS = {
