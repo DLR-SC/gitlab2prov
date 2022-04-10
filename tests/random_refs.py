7493e94f63bf7cdd6584263952ea1a6e1c4b1f90
cdboer <cdboer@cdboer.de>
2022-04-10 18:29:13 +0200

    Set AUTHOR as the default prov role for users

--- tests/random_refs.py
+++ tests/random_refs.py
@@ -1,6 +1,4 @@
-import inspect
 import uuid
-import random
 from gitlab2prov.domain import objects
 from gitlab2prov.domain.constants import ProvRole
 
@@ -10,14 +8,10 @@ def random_suffix():
 
 
 def random_user():
-    def isprop(v):
-        return isinstance(v, property)
-
     return objects.User(
         name=f"user-name-{random_suffix()}",
         email=f"user-email-{random_suffix()}",
         gitlab_username=f"gitlab-user-name-{random_suffix()}",
         gitlab_id=f"gitlab-user-id-{random_suffix()}",
-        prov_role=random.choice([value for (_, value) in inspect.getmembers(ProvRole, isprop)]),
+        prov_role=ProvRole.AUTHOR,
     )
-
