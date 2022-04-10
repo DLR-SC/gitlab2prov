8eb6b8f02976c879044aa4b7a360b12f0031c3ab
cdboer <cdboer@cdboer.de>
2022-04-10 17:10:43 +0200

    Set default log dest to stdout, allow log files
    Set the default output stream to stdout.
    Allow log files using FileHandlers.

--- gitlab2prov/log.py
+++ gitlab2prov/log.py
@@ -1,13 +1,18 @@
 import logging
+import sys
 
 
 LOG_LEVEL = logging.DEBUG
 LOG_FORMAT = "%(asctime)s %(levelname)s %(filename)s:%(funcName)s %(message)s"
 
 
-def create_logger(dest=logging.StreamHandler()):
+def create_logger(dest=None):
     logging.basicConfig(
-        filename=dest,
+        stream=sys.stdout,
         level=LOG_LEVEL,
         format=LOG_FORMAT,
     )
+    if dest is not None:
+        root = logging.getLogger("")
+        fileHandler = logging.FileHandler(dest)
+        root.addHandler(fileHandler)
