d7440140fb524dbb3c31bfe30cd6212d1947c6dd
cdboer <cdboer@cdboer.de>
2022-07-18 14:17:55 +0200

    Increase version number to 1.1.4

--- gitlab2prov/__init__.py
+++ gitlab2prov/__init__.py
@@ -3,5 +3,5 @@
 __author__ = "Claas de Boer, Andreas Schreiber"
 __copyright__ = "Copyright 2020, German Aerospace Center (DLR) and individual contributors"
 __license__ = "MIT"
-__version__ = "1.1.3"
+__version__ = "1.1.4"
 __status__ = "Development"
