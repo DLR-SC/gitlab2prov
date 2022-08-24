31078d5db7c33b063d8699881bf7b530c3c1afaa
cdboer <cdboer@cdboer.de>
2022-08-24 21:27:46 +0200

    Add AnnotationClassifier import

--- gitlab2prov/adapters/fetch/annotations/__init__.py
+++ gitlab2prov/adapters/fetch/annotations/__init__.py
@@ -1,5 +1,4 @@
-from gitlab2prov.adapters.fetch.annotations.classifiers import (
-    CLASSIFIERS,
-    IMPORT_STATEMENT,
-)
+from gitlab2prov.adapters.fetch.annotations.classifiers import CLASSIFIERS
+from gitlab2prov.adapters.fetch.annotations.classifiers import IMPORT_STATEMENT
+from gitlab2prov.adapters.fetch.annotations.classifiers import AnnotationClassifier
 from gitlab2prov.adapters.fetch.annotations.parse import parse_annotations
