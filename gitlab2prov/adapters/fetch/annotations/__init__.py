4651d7362267f5da507adbafbd17b3508bfc0a77
cdboer <cdboer@cdboer.de>
2023-01-29 18:17:06 +0100

    Remove fetch subpackage as it has been replaced by 'git', 'hub', 'lab' pckgs

--- gitlab2prov/adapters/fetch/annotations/__init__.py
+++
@@ -1,4 +0,0 @@
-from gitlab2prov.adapters.fetch.annotations.classifiers import CLASSIFIERS
-from gitlab2prov.adapters.fetch.annotations.classifiers import IMPORT_STATEMENT
-from gitlab2prov.adapters.fetch.annotations.classifiers import AnnotationClassifier
-from gitlab2prov.adapters.fetch.annotations.parse import parse_annotations
