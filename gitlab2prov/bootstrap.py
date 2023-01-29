19996b13b92c603e6331c48a25000131051f2264
cdboer <cdboer@cdboer.de>
2023-01-29 18:26:42 +0100

    Update bootstrapping to include the platform from which to fetch the project data (#79)

--- gitlab2prov/bootstrap.py
+++ gitlab2prov/bootstrap.py
@@ -1,23 +1,30 @@
 import inspect
 import logging
-from typing import Type
 
 from gitlab2prov.service_layer import handlers, messagebus, unit_of_work
-from gitlab2prov.adapters.fetch import GitFetcher, FetcherFactory
+
+from gitlab2prov.adapters.git import GitFetcher
+from gitlab2prov.adapters.lab import GitlabFetcher
+from gitlab2prov.adapters.hub import GithubFetcher
+from gitlab2prov.adapters.project_url import GithubProjectUrl, GitlabProjectUrl
 
 
 log = logging.getLogger(__name__)
 
 
 def bootstrap(
-    uow: unit_of_work.AbstractUnitOfWork = unit_of_work.InMemoryUnitOfWork(),
-    git_fetcher: Type[GitFetcher] = GitFetcher,
-    fetcher_factory: Type[FetcherFactory] = FetcherFactory,
+    platform: str,
+    uow: unit_of_work.UnitOfWork = unit_of_work.InMemoryUnitOfWork(),
+    git_fetcher: type[GitFetcher] = GitFetcher,
+    gitlab_fetcher: type[GitlabFetcher] = GitlabFetcher,
+    github_fetcher: type[GithubFetcher] = GithubFetcher,
+    github_url: type[GithubProjectUrl] = GithubProjectUrl,
+    gitlab_url: type[GitlabProjectUrl] = GitlabProjectUrl,
 ):
     dependencies = {
         "uow": uow,
-        "git_fetcher": git_fetcher,
-        "fetcher_factory": fetcher_factory,
+        "git_fetcher": git_fetcher(gitlab_url if platform == "gitlab" else github_url),
+        "githosted_fetcher": gitlab_fetcher if platform == "gitlab" else github_fetcher,
     }
     injected_handlers = {
         command_type: [inject_dependencies(handler, dependencies) for handler in handlers]
