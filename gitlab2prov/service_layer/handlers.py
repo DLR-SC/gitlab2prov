8c3ad8bf46da2fdbe41f6314cf78102c3b8417be
cdboer <cdboer@cdboer.de>
2023-01-29 18:25:47 +0100

    Add new handlers, update existing ones

--- gitlab2prov/service_layer/handlers.py
+++ gitlab2prov/service_layer/handlers.py
@@ -9,39 +9,56 @@ log = logging.getLogger(__name__)
 
 
 def fetch_git(cmd: commands.Fetch, uow, git_fetcher) -> None:
-    with git_fetcher(cmd.url, cmd.token) as fetcher:
-        fetcher.do_clone()
+    with git_fetcher as fetcher:
+        fetcher.do_clone(cmd.url, cmd.token)
         with uow:
-            for resource in fetcher.fetch_git():
+            for resource in fetcher.fetch_all():
                 log.info(f"add {resource=}")
-                uow.resources.add(resource)
+                uow.resources[cmd.url].add(resource)
         uow.commit()
 
 
-def fetch_githosted(cmd: commands.Fetch, uow, fetcher_factory) -> None:
-    fetcher = fetcher_factory.factory(cmd.url)
-    log.info("choose fetcher {fetcher=} for {cmd.url=}")
-    fetcher = fetcher(cmd.token, cmd.url)
+def fetch_githosted(cmd: commands.Fetch, uow, githosted_fetcher) -> None:
+    fetcher = githosted_fetcher(cmd.token, cmd.url)
     with uow:
         for resource in fetcher.fetch_all():
             log.info(f"add {resource=}")
-            uow.resources.add(resource)
+            uow.resources[cmd.url].add(resource)
         uow.commit()
-
-
-def reset(cmd: commands.Reset, uow):
-    log.info(f"reset repository {uow.resources=}")
-    uow.reset()
 
 
 def serialize(cmd: commands.Serialize, uow) -> ProvDocument:
     log.info(f"serialize graph consisting of {model.MODELS=}")
     document = ProvDocument()
     for prov_model in model.MODELS:
-        provenance = prov_model(uow.resources)
+        provenance = prov_model(uow.resources[cmd.url])
         document = operations.combine(document, provenance)
         document = operations.dedupe(document)
     return document
+
+
+def normalize(cmd: commands.Normalize):
+    if cmd.no_duplicates:
+        cmd.document = operations.dedupe(cmd.doc)
+    if cmd.use_pseudonyms:
+        cmd.document = operations.pseudonymize(cmd.doc)
+    return cmd.document
+
+
+def combine(cmd: commands.Combine):
+    return operations.combine(*cmd.documents)
+
+
+def document2file(cmd: commands.Document2File):
+    return operations.serialize(cmd.document, cmd.filename, cmd.format)
+
+
+def file2document(cmd: commands.File2Document):
+    return operations.deserialize(cmd.source, cmd.content, cmd.format)
+
+
+def statistics(cmd: commands.Statistics):
+    return operations.stats(cmd.document, cmd.resolution, cmd.format)
 
 
 HANDLERS = {
@@ -49,6 +66,10 @@ HANDLERS = {
         fetch_git,
         fetch_githosted,
     ],
-    commands.Reset: [reset],
     commands.Serialize: [serialize],
+    commands.Document2File: [document2file],
+    commands.File2Document: [file2document],
+    commands.Combine: [combine],
+    commands.Normalize: [normalize],
+    commands.Statistics: [statistics],
 }
