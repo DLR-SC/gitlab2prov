76596e1c778d797fcf4ebb510d7475be5c2777e2
cdboer <cdboer@cdboer.de>
2023-02-26 18:34:18 +0100

    Update commands

--- gitlab2prov/domain/commands.py
+++ gitlab2prov/domain/commands.py
@@ -6,35 +6,41 @@ from prov.model import ProvDocument
 
 @dataclass
 class Command:
+    """Base class for all commands."""
     pass
 
 
 @dataclass
 class Fetch(Command):
+    """Fetch data from cloned repository and remote projects."""
     url: str
     token: str
 
 
 @dataclass
 class Update(Fetch):
+    """Incremental update of data from cloned repository and remote projects."""
     last_updated_at: datetime
 
 
 @dataclass
-class Normalize(Command):
+class Transform(Command):
+    """Apply transformations to the provenance document."""
     document: ProvDocument
-    no_duplicates: bool = False
     use_pseudonyms: bool = False
-    agent_mapping: str = ""
+    remove_duplicates: bool = False
+    merge_aliased_agents: str = ""
 
  
 @dataclass
 class Combine(Command):
+    """Combine multiple provenance documents into one."""
     documents: list[ProvDocument]
 
 
 @dataclass
 class Statistics(Command):
+    """Calculate statistics for the provenance document."""
     document: ProvDocument
     resolution: str
     format: str
@@ -42,18 +48,21 @@ class Statistics(Command):
 
 @dataclass
 class Serialize(Command):
+    """Retrieve/Serialize provenance document from interal data store."""
     url: str = None
     
 
 @dataclass
-class Document2File(Command):
+class Write(Command):
+    """Write provenance document to file."""
     document: ProvDocument
     filename: Optional[str] = None
     format: Optional[str] = None
     
 
 @dataclass
-class File2Document(Command):
-    source: Optional[str] = None
+class Read(Command):
+    """Read provenance document from file."""
+    filename: Optional[str] = None
     content: Optional[str] = None
     format: Optional[str] = None
