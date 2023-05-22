b37fba789b88bbc464a9c6845a1fd9b2de7afc85
cdboer <dev@claas.plus>
2023-05-22 11:29:10 +0200

    Use pytest fixtures to generate random objects

--- tests/unit/test_repository.py
+++ tests/unit/test_repository.py
@@ -1,49 +1,49 @@
+import pytest
 from gitlab2prov.adapters.repository import InMemoryRepository
-from tests.random_generation import generate_random_users
 
 
 class TestInMemoryRepository:
 
-    users = generate_random_users(10)
 
-    def test_add_resource(self):
+    def test_add_resource(self, random_user):
         repo = InMemoryRepository()
-        resource = self.users[0]
+        resource = random_user 
         repo.add(resource)
         assert len(repo.repo[type(resource)]) == 1
         assert repo.repo[type(resource)][0] == resource
 
-    def test_get_resource_existing(self):
+    def test_get_resource_existing(self, random_user):
         repo = InMemoryRepository()
-        resource = self.users[0]
+        resource = random_user
         repo.add(resource)
         retrieved_resource = repo.get(type(resource))
         assert retrieved_resource == resource
 
-    def test_get_resource_non_existing(self):
+    def test_get_resource_non_existing(self, random_user):
         repo = InMemoryRepository()
-        retrieved_resource = repo.get(type(self.users[0]))
+        retrieved_resource = repo.get(type(random_user))
         assert retrieved_resource is None
 
-    def test_get_resource_with_filters_existing(self):
+    @pytest.mark.fixt_data(2)
+    def test_get_resource_with_filters_existing(self, n_random_users):
         repo = InMemoryRepository()
-        resource1 = self.users[0]
-        resource2 = self.users[1]
+        resource1 = n_random_users[0]
+        resource2 = n_random_users[1]
         repo.add(resource1)
         repo.add(resource2)
         retrieved_resource = repo.get(type(resource1), email=resource1.email, name=resource1.name)
         assert retrieved_resource == resource1
 
-    def test_get_resource_with_filters_non_existing(self):
+    def test_get_resource_with_filters_non_existing(self, random_user):
         repo = InMemoryRepository()
-        resource = self.users[0]
+        resource = random_user
         repo.add(resource)
         retrieved_resource = repo.get(type(resource), name="...", email="...")
         assert retrieved_resource is None
 
-    def test_get_resource_throws_attribute_error_for_non_existing_attributes(self):
+    def test_get_resource_throws_attribute_error_for_non_existing_attributes(self, random_user):
         repo = InMemoryRepository()
-        resource = self.users[0]
+        resource = random_user
         repo.add(resource)
         try:
             repo.get(type(resource), non_existing_attribute="...")
@@ -52,10 +52,11 @@ class TestInMemoryRepository:
         else:
             assert False
 
-    def test_list_all_resources(self):
+    @pytest.mark.fixt_data(2)
+    def test_list_all_resources(self, n_random_users):
         repo = InMemoryRepository()
-        resource1 = self.users[0]
-        resource2 = self.users[1]
+        resource1 = n_random_users[0]
+        resource2 = n_random_users[1]
         repo.add(resource1)
         repo.add(resource2)
         retrieved_resources = repo.list_all(type(resource1))
@@ -63,10 +64,11 @@ class TestInMemoryRepository:
         assert resource1 in retrieved_resources
         assert resource2 in retrieved_resources
 
-    def test_list_all_resources_with_filters_existing(self):
+    pytest.mark.fixt_data(2)
+    def test_list_all_resources_with_filters_existing(self, n_random_users):
         repo = InMemoryRepository()
-        resource1 = self.users[0]
-        resource2 = self.users[1]
+        resource1 = n_random_users[0]
+        resource2 = n_random_users[1]
         repo.add(resource1)
         repo.add(resource2)
         retrieved_resources = repo.list_all(
@@ -75,18 +77,19 @@ class TestInMemoryRepository:
         assert len(retrieved_resources) == 1
         assert resource1 in retrieved_resources
 
-    def test_list_all_resources_with_filters_non_existing(self):
+    @pytest.mark.fixt_data(2)
+    def test_list_all_resources_with_filters_non_existing(self, n_random_users):
         repo = InMemoryRepository()
-        resource1 = self.users[0]
-        resource2 = self.users[1]
+        resource1 = n_random_users[0]
+        resource2 = n_random_users[1]
         repo.add(resource1)
         repo.add(resource2)
         retrieved_resources = repo.list_all(type(resource1), name="...", email="...")
         assert len(retrieved_resources) == 0
 
-    def test_list_all_resources_throws_attribute_error_for_non_existing_attributes(self):
+    def test_list_all_resources_throws_attribute_error_for_non_existing_attributes(self, random_user):
         repo = InMemoryRepository()
-        resource1 = self.users[0]
+        resource1 = random_user
         repo.add(resource1)
         try:
             repo.list_all(type(resource1), non_existing_attribute="...")
