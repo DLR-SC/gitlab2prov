f8c306d2b3dcca4eb9ea1bbcb85bf5e41b3dddb1
cdboer <dev@claas.plus>
2023-09-05 16:22:29 +0200

    Bump version: 2.1.0 â†’ 2.2.0

--- gitlab2prov/__init__.py
+++ gitlab2prov/__init__.py
@@ -3,5 +3,5 @@
 __author__ = "Claas de Boer, Andreas Schreiber"
 __copyright__ = "Copyright 2020, German Aerospace Center (DLR) and individual contributors"
 __license__ = "MIT"
-__version__ = "2.1.0"
+__version__ = "2.2.0"
 __status__ = "Development"
